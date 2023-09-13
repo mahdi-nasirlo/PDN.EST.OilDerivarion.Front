8fe982c4ff5e9df51d1ac5d5b65d57f51c5e0310
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-09-13 15:26:55 +0330

    added page to the state-general-management

---
+++ src/app/state-general-management/requestdetail/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+
+import React from 'react'
+import PrimaryRequestsListForm from "@/app/state-general-management/requestdetail/components/primary-requests-form";
+
+
+export default function Page() {
+    return (
+        <>
+
+            <PrimaryRequestsListForm/>
+
+            {/*<PrimaryRequestsListTable/>*/}
+        </>
+    )
+}

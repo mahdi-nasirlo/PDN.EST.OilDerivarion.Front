f8b23a6c69791c569ddcb8830026e9a6fbd85488
morteza-vahdati <mvahdati1382@gmail.com>
2023-09-13 14:06:57 +0330

    created root and panel state-org-manager

---
+++ src/app/state-org-manager/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import AppLayout from "@/components/layout/layout";
+import { getMenuItem } from "@/components/layout/sidebar";
+import { HomeIcon } from "@heroicons/react/24/outline";
+import { MenuProps } from "antd";
+import Link from "next/link";
+
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            <AppLayout sidebarItems={items}>{children}</AppLayout>
+        </>
+    );
+}
+
+
+const items: MenuProps["items"] = [
+    getMenuItem(
+        <Link href="/state-org-manager">خانه</Link>,
+        "/state-org-manager",
+        <HomeIcon width={16} height={16} />
+    ),
+    { type: "divider" },
+];

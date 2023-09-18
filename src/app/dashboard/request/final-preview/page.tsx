6d484b8169aed948e1e316861edc43bcd9fc4d5c
madhi-nasirlo <m.nwordpress1223@gmail.com>
2023-09-18 14:13:27 +0330

    update ui

---
+++ src/app/dashboard/request/final-preview/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import ReviewDataTable from "@/app/dashboard/request/final-preview/components/review-data-table";
+
+
+export default function Page() {
+
+
+    return (
+        <>
+            <ReviewDataTable/>
+        </>
+    );
+}

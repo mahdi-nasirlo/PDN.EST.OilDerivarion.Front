d00d43f3afe5d513b1ed840a0c096ddd71bd9c21
morteza-vahdati <mvahdati1382@gmail.com>
2023-09-18 11:49:31 +0330

    Added DatePicker to the necessary inputs

--- src/app/admin-pannel/test-factors/page.tsx
+++ src/app/admin-pannel/test-factors/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useForm } from 'antd/es/form/Form';
 import React, { useState } from 'react';
 import PrimaryTestFactorsForm from './components/primary-test-factors-form';
 import PrimaryTestFactorsTable from './components/primary-test-factors-table';
@@ -9,25 +8,7 @@ import PrimaryTestFactorsModal from './components/primary-test-factors-modal';
 export default function Page() {
     const [modalVisible, setModalVisible] = useState(false);
 
-    const [form] = useForm()
-
-    const showModal = () => {
-        setModalVisible(true);
-    };
-
-    const closeModal = () => {
-        setModalVisible(false);
-    };
-
-    const handleFormSubmit = async () => {
-        try {
-            const values = await form.validateFields();
-            console.log("Form values:", values); // Log the form values to the console
-            closeModal();
-        } catch (error) {
-            console.error("Form validation failed:", error);
-        }
-    }; return (
+    return (
         <>
             <PrimaryTestFactorsForm />
             <PrimaryTestFactorsTable setModalVisible={setModalVisible} />

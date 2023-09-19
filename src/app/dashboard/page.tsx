c5712c129f2b3c826afaa7dc7a2f614f19ccab75
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2023-09-19 13:50:40 +0330

    started provincial-working-group pannnel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,81 +2,89 @@ import DisplayData from "@/app/dashboard/components/showData";
 import { Typography } from "antd";
 import Link from "next/link";
 
-
 export default async function Home() {
+  const data = await getData();
 
-    const data = await getData()
+  return (
+    <>
+      <div className="box-border w-full lg:mt-8 lg:p-6 p-2 mt-3">
+        <DisplayData data={data} />
+        <div className="flex">
+          <Typography className="text-1xl">ورود به پنل</Typography>
+          <Typography>
+            <Link
+              className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
+              href="/manufacturer"
+            >
+              رییس اجرایی
+            </Link>
+          </Typography>
+        </div>
+        <div className="flex">
+          <Typography className="text-1xl">ورود به پنل</Typography>
+          <Typography>
+            <Link
+              className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
+              href="/admin-pannel"
+            >
+              ادمین
+            </Link>
+          </Typography>
+        </div>
+        <div className="flex">
+          <Typography className="text-1xl">ورود به پنل</Typography>
+          <Typography>
+            <Link
+              className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
+              href="/state-general-management"
+            >
+              پنل مدیر کل استان
+            </Link>
+          </Typography>
+        </div>
+        <div className="flex">
+          <Typography className="text-1xl">ورود به پنل</Typography>
+          <Typography>
+            <Link
+              className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
+              href="/state-org-manager"
+            >
+              پنل اداره کل استان
+            </Link>
+          </Typography>
+        </div>
+        <div className="flex">
+          <Typography className="text-1xl">ورود به پنل</Typography>
 
-
-    return (
-        <>
-            <div className="box-border w-full lg:mt-8 lg:p-6 p-2 mt-3">
-                <DisplayData data={data} />
-                <div className="flex">
-                    <Typography className="text-1xl">
-                        ورود به پنل
-                    </Typography>
-                    <Typography>
-                        <Link className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
-                            href="/manufacturer">
-                            رییس اجرایی
-                        </Link>
-                    </Typography>
-                </div>
-                <div className="flex">
-                    <Typography className="text-1xl">
-                        ورود به پنل
-                    </Typography>
-                    <Typography>
-                        <Link className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
-                            href="/admin-pannel">
-                            ادمین
-                        </Link>
-                    </Typography>
-                </div>
-                <div className="flex">
-                    <Typography className="text-1xl">
-                        ورود به پنل
-                    </Typography>
-                    <Typography>
-                        <Link className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
-                            href="/state-general-management">
-                            پنل مدیر کل استان
-                        </Link>
-                    </Typography>
-                </div>
-                <div className="flex">
-                    <Typography className="text-1xl">
-                        ورود به پنل
-                    </Typography>
-                    <Typography>
-                        <Link className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
-                            href="/state-org-manager">
-                            پنل اداره کل استان
-                        </Link>
-                    </Typography>
-                </div>
-            </div>
-        </>
-    )
+          <Typography>
+            <Link
+              className="mx-2 underline-offset-2 text-primary-500 text-1xl font-extrabold"
+              href="/provincial-working-group"
+            >
+              پنل کار کروه استان
+            </Link>
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
 }
 
 async function getData() {
+  // const cookieStore = cookies()
+  // const token = cookieStore.get('accessToken')?.value
 
-    // const cookieStore = cookies()
-    // const token = cookieStore.get('accessToken')?.value
+  // const res = await fetch(`${process.env['NEXT_PUBLIC_API_URL']}/security/getId`, {
+  //     method: "POST",
+  //     headers: {
+  //         "Content-Type": 'application/json',
+  //         "Authorization": `Bearer ${token}`
+  //     }
+  // })
 
-    // const res = await fetch(`${process.env['NEXT_PUBLIC_API_URL']}/security/getId`, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": 'application/json',
-    //         "Authorization": `Bearer ${token}`
-    //     }
-    // })
+  // if (res.ok) {
+  //     return res.json()
+  // }
 
-    // if (res.ok) {
-    //     return res.json()
-    // }
-
-    return null
+  return null;
 }
---
+++ src/app/provincial-working-group/layout.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import AppLayout from "@/components/layout/layout";
+import { getMenuItem } from "@/components/layout/sidebar";
+import { HomeIcon } from "@heroicons/react/24/outline";
+import { FolderPlusIcon } from "@heroicons/react/24/solid";
+import { MenuProps } from "antd";
+import Link from "next/link";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <AppLayout sidebarItems={items}>{children}</AppLayout>
+    </>
+  );
+}
+
+const items: MenuProps["items"] = [
+  getMenuItem(
+    <Link href={"/state-general-management"}>خانه</Link>,
+    "state-general-management",
+    <HomeIcon width={16} height={16} />
+  ),
+
+  getMenuItem(
+    "پیشخوان",
+    "state-general-management",
+    null,
+    [
+      getMenuItem(
+        <Link href={"/provincial-working-group/Invitations"}>
+          دعوت نامه ها
+        </Link>,
+        "requests-list",
+        <FolderPlusIcon width={16} height={16} />
+      ),
+      //   getMenuItem(
+      //     <Link href={"/state-general-management/delays-list"}>تاخیر ها</Link>,
+      //     "delays-list",
+      //     <FolderPlusIcon width={16} height={16} />
+      //   ),
+    ],
+    "group"
+  ),
+  //   getMenuItem(
+  //     <Link href={"/state-general-management/requestdetail"}>
+  //       جزِییات درخواست
+  //     </Link>,
+  //     "requestdetail",
+  //     <FolderPlusIcon width={16} height={16} />
+  //   ),
+  //   getMenuItem(
+  //     <Link href={"/state-general-management/requests-list"}>
+  //       ارتباط با پشتیبانی
+  //     </Link>,
+  //     "requests-list",
+  //     <FolderPlusIcon width={16} height={16} />
+  //   ),
+];

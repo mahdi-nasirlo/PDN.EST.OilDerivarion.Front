3d1badaaf5fce54b8d9f83d40f4e7bac7a49ae1c
morteza-vahdati <mvahdati1382@gmail.com>
2023-09-25 08:59:32 +0330

    created requests-detail in panel laboratory

---
+++ src/app/laboratory/request-details/components/data-table.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+
+import { Divider, Table, Typography } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+import React from 'react'
+
+
+interface DataType {
+    key: string;
+    Row: number;
+    name: string;
+}
+
+
+export default function DataTable() {
+
+    const columns: ColumnsType<DataType> = [
+        {
+            title: "ردیف",
+            dataIndex: "Row",
+            key: "1",
+        },
+        {
+            title: "نام فاکتور آزمون",
+            dataIndex: "name",
+            key: "2",
+        },
+    ];
+
+
+
+    return (
+        <>
+            <Divider />
+            <Typography className="mt-3 mb-6 text-right font-medium text-base text-secondary-500 text-secondary">
+                فاکتور های آزمون
+            </Typography>
+            <Table
+                className="mt-8"
+                columns={columns}
+                dataSource={data}
+                pagination={false}
+            />
+        </>
+    )
+}
+
+
+
+const data: DataType[] = [
+    {
+        key: "1",
+        Row: 1,
+        name: "نام فاکتور آزمون تستی",
+    },
+    {
+        key: "2",
+        Row: 2,
+        name: "نام فاکتور آزمون تستی",
+    },
+];

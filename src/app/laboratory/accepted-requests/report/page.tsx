720a720716d1b388eafc6b541c47d54ab91c1751
madhi-nasirlo <m.nwordpress1223@gmail.com>
2023-10-07 14:23:08 +0330

    create gps confirmation page

---
+++ src/app/laboratory/accepted-requests/report/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import FormMaterial from "./components/form-material";
+
+
+export default function Page() {
+
+
+    return (
+        <>
+            <div className="box-border w-full p-6">
+                <FormMaterial/>
+            </div>
+        </>
+    )
+}

8326e06df2fe3d658fb03e149d2a874a66b41065
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2023-10-10 08:55:23 +0330

    fixed layout form

--- src/app/admin-panel/management-user/components/create-modal.tsx
+++ src/app/admin-panel/management-user/components/create-modal.tsx
@@ -1,144 +1,128 @@
 "use client";
 
-import { Button, Col, Form, Input, Modal, Row, Select } from 'antd'
-import { useForm } from 'antd/es/form/Form';
-import React from 'react'
+import { Button, Col, Form, Input, Modal, Row, Select } from "antd";
+import { useForm } from "antd/es/form/Form";
+import React from "react";
 
+export default function CreateModal({
+  modalVisible,
+  setModalVisible,
+}: {
+  modalVisible: any;
+  setModalVisible: any;
+}) {
+  const [form] = useForm();
 
-export default function CreateModal({ modalVisible, setModalVisible }: { modalVisible: any, setModalVisible: any }) {
+  const closeModal = () => {
+    setModalVisible(false);
+  };
 
-    const [form] = useForm()
+  const handleFormSubmit = async () => {
+    try {
+      const values = await form.validateFields();
+      console.log("Form values:", values); // Log the form values to the console
+      closeModal();
+    } catch (error) {
+      console.error("Form validation failed:", error);
+    }
+  };
 
-    const closeModal = () => {
-        setModalVisible(false);
-    };
-
-    const handleFormSubmit = async () => {
-        try {
-            const values = await form.validateFields();
-            console.log("Form values:", values); // Log the form values to the console
-            closeModal();
-        } catch (error) {
-            console.error("Form validation failed:", error);
-        }
-    };
-
-
-    return (
-        <Modal
-            width={800}
-            title={<div>
-                <div className="text-base mb-2">افزودن کاربر جدید</div>
-                <div className="font-normal text-sm">لطفا اطلاعات را وارد نمایید.</div>
-            </div>}
-            visible={modalVisible}
-            onCancel={closeModal}
-            footer={[
-                <Row key={"box"} gutter={[16, 16]} className="my-2">
-                    <Col xs={24} md={12}>
-                        <Button
-                            size="large"
-                            className="w-full"
-                            type="primary"
-                            onClick={handleFormSubmit}
-                            key={"submit"} >
-                            ثبت
-                        </Button >
-                    </Col>
-                    <Col xs={24} md={12}>
-                        <Button
-                            size="large"
-                            className="w-full bg-gray-100 text-warmGray-500"
-                            onClick={closeModal}
-                            key={"cancel"} >
-                            انصراف
-                        </Button >
-                    </Col>
-                </Row>
-            ]}
-        >
-            <Form form={form} >
-                <Row gutter={[32, 1]}>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            name="year-establishment" label="نام">
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                    <Col xs={24} md={12}>
-                        <Form.Item labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            name="lastName" label="نام خانوادگی">
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={[32, 1]}>
-                    <Col xs={24} md={12}>
-                        <Form.Item labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            name="year-establishment" label="کد ملی">
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                    <Col xs={24} md={12}>
-                        <Form.Item labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }} name="lastName" label="شناسه کاربری">
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={[32, 1]}>
-                    <Col xs={24} md={12}>
-                        <Form.Item labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            name="year-establishment" label="شماره تماس">
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                    <Col xs={24} md={12}>
-                        <Form.Item labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            name="lastName" label="استان">
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={[32, 1]}>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            name="year-establishment"
-                            label="شهر"
-                        >
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            name="year-establishment"
-                            label="فعال/غیر فعال"
-                        >
-                            <Select size="large" placeholder="انتخاب کنید" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={[32, 1]}>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            name="year-establishment" label="نقش">
-                            <Select size="large" placeholder="انتخاب کنید" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-            </Form>
-        </Modal >
-    )
+  return (
+    <Modal
+      width={800}
+      title={
+        <div>
+          <div className="text-base mb-2">افزودن کاربر جدید</div>
+          <div className="font-normal text-sm">
+            لطفا اطلاعات را وارد نمایید.
+          </div>
+        </div>
+      }
+      visible={modalVisible}
+      onCancel={closeModal}
+      footer={[
+        <Row key={"box"} gutter={[16, 16]} className="my-2">
+          <Col xs={24} md={12}>
+            <Button
+              size="large"
+              className="w-full"
+              type="primary"
+              onClick={handleFormSubmit}
+              key={"submit"}
+            >
+              ثبت
+            </Button>
+          </Col>
+          <Col xs={24} md={12}>
+            <Button
+              size="large"
+              className="w-full bg-gray-100 text-warmGray-500"
+              onClick={closeModal}
+              key={"cancel"}
+            >
+              انصراف
+            </Button>
+          </Col>
+        </Row>,
+      ]}
+    >
+      <Form form={form} layout="vertical">
+        <Row gutter={[32, 1]}>
+          <Col xs={24} md={12}>
+            <Form.Item name="year-establishment" label="نام">
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={12}>
+            <Form.Item name="lastName" label="نام خانوادگی">
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={[32, 1]}>
+          <Col xs={24} md={12}>
+            <Form.Item name="year-establishment" label="کد ملی">
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={12}>
+            <Form.Item name="lastName" label="شناسه کاربری">
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={[32, 1]}>
+          <Col xs={24} md={12}>
+            <Form.Item name="year-establishment" label="شماره تماس">
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={12}>
+            <Form.Item name="lastName" label="استان">
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={[32, 1]}>
+          <Col xs={24} md={12}>
+            <Form.Item name="year-establishment" label="شهر">
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={12}>
+            <Form.Item name="year-establishment" label="فعال/غیر فعال">
+              <Select size="large" placeholder="انتخاب کنید" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={[32, 1]}>
+          <Col xs={24} md={12}>
+            <Form.Item name="year-establishment" label="نقش">
+              <Select size="large" placeholder="انتخاب کنید" />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
+  );
 }
--- src/app/admin-panel/product/products-list/components/product-form.tsx
+++ src/app/admin-panel/product/products-list/components/product-form.tsx
@@ -1,61 +1,74 @@
-import React from 'react';
+import React from "react";
 import { Col, Form, Row, Select } from "antd";
 import useSWR from "swr";
 import { listFetcher } from "../../../../../../lib/server/listFetcher";
 import { filterOption } from "../../../../../../lib/filterOption";
 
 function ProductForm() {
+  const { data: products, isLoading: ldProducts } = useSWR(
+    [
+      "/Product/GetAll",
+      {
+        name: null,
+        is_Active: null,
+      },
+    ],
+    ([url, arg]: [string, any]) => listFetcher(url, { arg })
+  );
 
-    const { data: products, isLoading: ldProducts } = useSWR(["/Product/GetAll", {
-        "name": null,
-        "is_Active": null
-    }], ([url, arg]: [string, any]) => listFetcher(url, { arg }))
+  const { data: material, isLoading: ldMaterial } = useSWR(
+    [
+      "/Material/GetAll",
+      {
+        name: null,
+        is_Active: null,
+      },
+    ],
+    ([url, arg]: [string, any]) => listFetcher(url, { arg })
+  );
 
-    const { data: material, isLoading: ldMaterial } = useSWR(["/Material/GetAll", {
-        "name": null,
-        "is_Active": null
-    }], ([url, arg]: [string, any]) => listFetcher(url, { arg }))
-
-    return (
-        <>
-            <Row gutter={[32, 1]}>
-                <Col xs={24} md={12}>
-                    <Form.Item
-                        rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
-                        name="productUid"
-                        label="نام محصول"
-                    >
-                        <Select
-                            showSearch
-                            fieldNames={{ label: "Name", value: "Uid" }}
-                            // @ts-ignore
-                            filterOption={filterOption}
-                            loading={ldProducts}
-                            options={products}
-                            size="large"
-                            placeholder="انتخاب کنید" />
-                    </Form.Item>
-                </Col>
-                <Col xs={24} md={12}>
-                    <Form.Item
-                        rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
-                        name="materialUid"
-                        label="مواد اولیه"
-                    >
-                        <Select
-                            showSearch
-                            fieldNames={{ label: "Name", value: "Uid" }}
-                            // @ts-ignore
-                            filterOption={filterOption}
-                            loading={ldMaterial}
-                            options={material}
-                            size="large"
-                            placeholder="انتخاب کنید" />
-                    </Form.Item>
-                </Col>
-            </Row>
-        </>
-    )
+  return (
+    <>
+      <Row gutter={[32, 1]}>
+        <Col xs={24} md={12}>
+          <Form.Item
+            rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
+            name="productUid"
+            label="نام محصول"
+          >
+            <Select
+              showSearch
+              fieldNames={{ label: "Name", value: "Uid" }}
+              // @ts-ignore
+              filterOption={filterOption}
+              loading={ldProducts}
+              options={products}
+              size="large"
+              placeholder="انتخاب کنید"
+            />
+          </Form.Item>
+        </Col>
+        <Col xs={24} md={12}>
+          <Form.Item
+            rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
+            name="materialUid"
+            label="مواد اولیه"
+          >
+            <Select
+              showSearch
+              fieldNames={{ label: "Name", value: "Uid" }}
+              // @ts-ignore
+              filterOption={filterOption}
+              loading={ldMaterial}
+              options={material}
+              size="large"
+              placeholder="انتخاب کنید"
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default ProductForm;

f42ecde9e9f200c9866553f7889e30089427fe05
morteza-vahdati <mvahdati1382@gmail.com>
2023-10-18 16:28:15 +0330

    fixed Producer-List in manufacturer panel

--- interfaces/producer.ts
+++ interfaces/producer.ts
@@ -1,31 +1,46 @@
 export type GetPage_ExeManager = {
-    records: Person[],
-    count: number,
-}
+  records: Person[];
+  count: number;
+};
 
 export type Get_ExeManager = {
-    data: Person,
-    choices: { label: string, key: string }[],
-}
+  data: Person;
+  choices: { label: string; key: string }[];
+};
 
 export type Person = {
-    row: number,
-    name: string,
-    nationalCode: string,
-    ceoName: string,
-    companyOwnershipTypeName: string
-}
+  row: number;
+  name: string;
+  nationalCode: string;
+  ceoName: string;
+  companyOwnershipTypeName: string;
+};
 
 export type Employee = {
-    companyRoleId: number,
-    companyRoleName: string,
-    memberName: string,
-    memberNationalCode: string
-}
+  companyRoleId: number;
+  companyRoleName: string;
+  memberName: string;
+  memberNationalCode: string;
+};
 
 export type Ceo = {
-    companyRoleId: number,
-    memberNationalCode: string,
-    companyRoleName: string,
-    memberName: string
-}
+  companyRoleId: number;
+  memberNationalCode: string;
+  companyRoleName: string;
+  memberName: string;
+};
+
+export type GetPageProducer = {
+  name: string | null;
+  companyStatusId: string | number | null;
+  fromRecord: number;
+  selectRecord: number;
+};
+
+export type ProducerRecords = {
+  Row?: number | null;
+  companyOwnershipTypeName: string | null;
+  companyStatusName: string | null;
+  name: string | null;
+  nationalCode: string | null;
+};
--- src/app/manufacturer/creator-list/page.tsx
+++ src/app/manufacturer/creator-list/page.tsx
@@ -1,18 +1,58 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Collapse } from "antd";
 import FilterForm from "./components/filter-form";
 import DataTable from "./components/data-table";
+import { listFetcher } from "../../../../lib/server/listFetcher";
+import useSWR from "swr";
+import { GetPageProducer, ProducerRecords } from "../../../../interfaces/producer";
+
 
 export default function Page() {
+
+    const defaultValueTable = {
+        name: null,
+        companyStatusId: null,
+        fromRecord: 0,
+        selectRecord: 10000
+    }
+
+    const [filter, setFilter] = useState(defaultValueTable)
+
+    const { data: ProducerList, isLoading: ldProducerList, mutate } = useSWR<{
+        records: ProducerRecords[];
+        count: number;
+    }>(
+        ["/Company/GetPageProducer", filter],
+        ([url, arg]: [string, any]) => listFetcher(url, { arg })
+    );
+
+    const setFilterTable = async (values: GetPageProducer) => {
+        //@ts-ignore
+        setFilter({ name: values.name, companyStatusId: values.companyStatusId, fromRecord: 0, selectRecord: 1000 })
+
+        await mutate()
+    }
+
+    const unsetFilter = async () => {
+
+        setFilter(defaultValueTable)
+
+        await mutate()
+    }
+
     return (
         <>
             <Collapse
                 size="large"
-                items={[{ label: 'فیلتر جدول', children: <FilterForm /> }]}
+                items={[{ label: 'فیلتر جدول', children: <FilterForm unsetFilter={unsetFilter} filter={setFilterTable} /> }]}
             />
-            <DataTable />
+            <DataTable
+                mutate={mutate}
+                ProducerList={ProducerList?.records}
+                ldProducerList={ldProducerList}
+            />
         </>
     );
 }

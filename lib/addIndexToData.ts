9d3331f8760ea2b4dab6e1f6f5ad74d5ebe07f59
madhi-nasirlo <m.nwordpress1223@gmail.com>
2023-10-29 17:38:17 +0330

    FIX:: pagination mistake

--- lib/addIndexToData.ts
+++ lib/addIndexToData.ts
@@ -1,9 +1,9 @@
-export const addIndexToData = (data: any[] | undefined, keyName: string = "Row") => {
+export const addIndexToData = (data: any[] | undefined, keyName: string = "Row", startFrom: number = 1) => {
     if (!data) return [];
 
-    // Use `map` to add an index to each item in the data array
+
     return data.map((item: any, index: number) => ({
         ...item,
-        [keyName]: index + 1, // Auto-incrementing index starting from 1
+        [keyName]: index + startFrom + 1, // Auto-incrementing index starting from 1
     }));
 };

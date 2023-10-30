9bb92de0774f266a864ee8318ce5ac5eb552d225
madhi-nasirlo <m.nwordpress1223@gmail.com>
2023-10-30 15:32:48 +0330

    FIX:: fix some issue

--- src/app/admin-panel/measures/components/measure-form.tsx
+++ src/app/admin-panel/measures/components/measure-form.tsx
@@ -1,33 +1,31 @@
 import React from "react";
-import { Col, Form, Input, InputNumber, Row, Select } from "antd";
-import useSWR from "swr";
-import { listFetcher } from "../../../../../lib/server/listFetcher";
+import {Col, Form, Input, Row, Select} from "antd";
 
 function MeasureForm() {
   return (
     <>
       <Row gutter={[32, 1]}>
-        <Col xs={24} md={12}>
-          <Form.Item
-            name="name"
-            label="واحد اندازه گیری"
-            rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
-          >
-            <Input size="large" placeholder="وارد کنید" />
-          </Form.Item>
-        </Col>
-        <Col xs={24} md={12}>
-          <Form.Item name="isActive" label="فعال/غیرفعال">
-            <Select
-              options={[
-                { label: "فعال", value: true },
-                { label: "غیرفعال", value: false },
-              ]}
-              size="large"
-              placeholder="انتخاب کنید"
-            />
-          </Form.Item>
-        </Col>
+          <Col xs={24} md={12}>
+              <Form.Item
+                  name="name"
+                  label="واحد اندازه گیری"
+                  rules={[{required: true}]}
+              >
+                  <Input size="large" placeholder="وارد کنید"/>
+              </Form.Item>
+          </Col>
+          <Col xs={24} md={12}>
+              <Form.Item name="isActive" label="فعال/غیرفعال" rules={[{required: true}]}>
+                  <Select
+                      options={[
+                          {label: "فعال", value: true},
+                          {label: "غیرفعال", value: false},
+                      ]}
+                      size="large"
+                      placeholder="انتخاب کنید"
+                  />
+              </Form.Item>
+          </Col>
       </Row>
     </>
   );

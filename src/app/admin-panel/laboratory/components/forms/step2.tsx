3d1e35f5f82f1e1186979f089073a7a663ba377a
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2023-10-31 11:44:22 +0330

    validdate the labaratory create modal

--- src/app/admin-panel/laboratory/components/forms/step2.tsx
+++ src/app/admin-panel/laboratory/components/forms/step2.tsx
@@ -1,57 +1,69 @@
-import React from 'react';
+import React from "react";
 import { Col, Form, FormInstance, Input, Row } from "antd";
 
-function Step2({ form, handleSubmit, isLoading }: {
-    isLoading: boolean,
-    form: FormInstance,
-    handleSubmit: (values: SaveFormResponsible) => void
+function Step2({
+  form,
+  handleSubmit,
+  isLoading,
+}: {
+  isLoading: boolean;
+  form: FormInstance;
+  handleSubmit: (values: SaveFormResponsible) => void;
 }) {
-    return (
-        <>
-            <Form form={form} onFinish={handleSubmit} disabled={isLoading} layout='vertical'>
-                <Row gutter={[32, 1]}>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
-                            name="responsibleFirstName"
-                            label="نام"
-                        >
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
-                            name="responsibleLastName"
-                            label="نام خانوادگی"
-                        >
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={[32, 1]}>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
-                            name="responsibleNationalCode"
-                            label="کد ملی"
-                        >
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
-                            name="responsibleMobile"
-                            label="شماره موبایل"
-                        >
-                            <Input size="large" placeholder="وارد کنید" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-            </Form>
-        </>
-    );
+  return (
+    <>
+      <Form
+        form={form}
+        onFinish={handleSubmit}
+        disabled={isLoading}
+        layout="vertical"
+      >
+        <Row gutter={[32, 1]}>
+          <Col xs={24} md={12}>
+            <Form.Item
+              rules={[{ required: true }]}
+              name="responsibleFirstName"
+              label="نام"
+            >
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={12}>
+            <Form.Item
+              rules={[{ required: true }]}
+              name="responsibleLastName"
+              label="نام خانوادگی"
+            >
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={[32, 1]}>
+          <Col xs={24} md={12}>
+            <Form.Item
+              rules={[
+                { required: true },
+                { min: 10, max: 10, message: "کد ملی باید ده رقمی باشد" },
+              ]}
+              name="responsibleNationalCode"
+              label="کد ملی"
+            >
+              <Input size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={12}>
+            <Form.Item
+              rules={[{ required: true }]}
+              name="responsibleMobile"
+              label="شماره موبایل"
+            >
+              <Input type="number" size="large" placeholder="وارد کنید" />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 }
 
 export default Step2;

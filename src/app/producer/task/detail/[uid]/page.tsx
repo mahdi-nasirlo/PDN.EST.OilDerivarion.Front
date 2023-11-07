60b557223001d7fe9caa8c8f81e8d3f072e9d961
madhi-nasirlo <m.nwordpress1223@gmail.com>
2023-11-07 19:30:05 +0330

    ADD:: complete page with viewer

--- src/app/producer/task/detail/[uid]/page.tsx
+++ src/app/producer/task/detail/[uid]/page.tsx
@@ -3,14 +3,30 @@
 import useSWR from "swr";
 import {listFetcher} from "../../../../../../lib/server/listFetcher";
 import WorkflowDataViewer from "../../../../../../components/WorkflowDataViewer";
+import WorkflowRequestBtn from "../../../../../../components/WorkflowRequestBtn";
+import {Choice} from "../../../../../../interfaces/requestDetail";
+import {Divider} from "antd";
 
 interface PropType {
     params: { uid: string }
 }
 
+interface DataFetchType {
+    choices: Choice[],
+    task: {
+        processId: string,
+        stepId: string,
+        reference_ID: string,
+        group_ID: string,
+        step_Name: string,
+        counting_position: string,
+        userId: number
+    }
+}
+
 export default function Home(props: PropType) {
 
-    const {isLoading, data, mutate} = useSWR("/WorkFlowRequest/GetStep02", (url) => listFetcher(url, {
+    const {isLoading, data, mutate} = useSWR<DataFetchType>("/WorkFlowRequest/GetStep02", (url) => listFetcher(url, {
         arg: {
             taskId: props.params.uid
         }
@@ -20,6 +36,9 @@ export default function Home(props: PropType) {
         <>
             <div className="box-border w-full p-6">
                 <WorkflowDataViewer data={data}/>
+                <Divider/>
+                <WorkflowRequestBtn choices={data?.choices} nextStepUrl={"/WorkFlowRequest/SetStep02"}
+                                    taskId={data?.task.stepId}/>
             </div>
         </>
     );

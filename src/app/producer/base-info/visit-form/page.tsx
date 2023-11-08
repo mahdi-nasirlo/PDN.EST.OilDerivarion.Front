e890164f94bd26161170774d747d38e625c442d8
morteza-vahdati <mvahdati1382@gmail.com>
2023-11-08 14:20:17 +0330

    fixed bugs

--- src/app/producer/base-info/visit-form/page.tsx
+++ src/app/producer/base-info/visit-form/page.tsx
@@ -8,9 +8,9 @@ import Step2 from "./forms/step2";
 import Step3 from "./forms/step3";
 import Step4 from "./forms/step4";
 import Step5 from "./forms/step5";
-// import Step6 from './forms/step6';
-// import Step7 from './forms/step7';
-// import Step8 from './forms/step8';
+import Step6 from './forms/step6';
+import Step7 from './forms/step7';
+import Step8 from './forms/step8';
 
 function Page() {
   const processController = useControlSteps();
@@ -33,15 +33,15 @@ function Page() {
     case 4:
       CurrentStep = <Step5 />;
       break;
-    // case 5:
-    //     CurrentStep = <Step6 />
-    //     break;
-    // case 6:
-    //     CurrentStep = <Step7 />
-    //     break;
-    // case 7:
-    //     CurrentStep = <Step8 />
-    //     break;
+    case 5:
+      CurrentStep = <Step6 />
+      break;
+    case 6:
+      CurrentStep = <Step7 />
+      break;
+    case 7:
+      CurrentStep = <Step8 />
+      break;
     default:
       CurrentStep = <></>;
   }

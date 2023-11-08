39881b52d7efe6514444b7d8ebdd79652602c728
morteza-vahdati <mvahdati1382@gmail.com>
2023-11-08 10:28:56 +0330

    Created the visit-form step2 in basic-info

---
+++ src/app/producer/activate/visit-form/forms/step2/index.tsx
@@ -0,0 +1,51 @@
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon, PlusIcon } from '@heroicons/react/24/outline'
+import { Button, Divider, Form, Typography } from 'antd'
+import React, { useContext, useState } from 'react'
+import StepContext from '../../stete-manager/step-context';
+import { useForm } from 'antd/es/form/Form';
+import DataTable from './components/data-table';
+import CreateForm from './components/create-form';
+
+export default function Index() {
+
+    const processController = useContext(StepContext);
+
+    return (
+        <>
+            <div className='flex justify-between'>
+                <div className='flex items-center gap-3'>
+                    <Typography className='font-bold'>اطلاعات خط تولید</Typography>
+                    <Typography className='text-secondary-500'>
+                        اطلاعات مخازن محصول ( 2 از 8 )
+                    </Typography>
+                </div>
+            </div >
+            <Divider />
+            <CreateForm />
+            <DataTable />
+            <Divider />
+            <div className='flex gap-3'>
+                <Button
+                    className="w-full bg-gray-100"
+                    size="large"
+                    type="dashed"
+                    htmlType="submit"
+                    onClick={() => processController.dispatch({ type: "PREVIOUS" })}
+                >
+                    مرحله قبلی
+                </Button>
+                <Button
+                    className="w-full management-info-form-submit btn-filter"
+                    size="large"
+                    type="primary"
+                    htmlType="submit"
+                    onClick={() =>
+                        processController.dispatch({ type: "NEXT", stepNumber: 7 })
+                    }
+                >
+                    مرحله بعد
+                </Button>
+            </div>
+        </>
+    )
+}

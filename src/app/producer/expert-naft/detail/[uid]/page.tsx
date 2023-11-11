746729ef5caf90307f3c40f29c19f143a3c8e431
madhi-nasirlo <m.nwordpress1223@gmail.com>
2023-11-11 17:59:45 +0330

    ADD:: create step 02

--- src/app/producer/expert-naft/detail/[uid]/page.tsx
+++ src/app/producer/expert-naft/detail/[uid]/page.tsx
@@ -1,159 +1,105 @@
 "use client";
 
-import useSWR from "swr";
-
-import { Button, Col, Divider, Form, Input, Modal, Row } from "antd";
-import { listFetcher } from "../../../../../../lib/server/listFetcher";
-import { Choice } from "../../../../../../interfaces/requestDetail";
-import WorkflowDataViewer from "../../../../../../components/WorkflowDataViewer";
-import WorkflowRequestBtn from "../../../../../../components/WorkflowRequestBtn";
-import { addIndexToData } from "../../../../../../lib/addIndexToData";
-import CustomeDatePicker from "../../../../../../components/CustomeDatePicker";
+import {Button, Divider} from "antd";
+import {Choice} from "../../../../../../interfaces/requestDetail";
+import WorkflowDataViewer from "../../../../../../components/Workflow/WorkflowDataViewer";
 import useSWRMutation from "swr/mutation";
-import { mutationFetcher } from "../../../../../../lib/server/mutationFetcher";
-import { useForm } from "antd/es/form/Form";
-import { useState } from "react";
-import { serialize } from "v8";
-import { useRouter } from "next/navigation";
+import {mutationFetcher} from "../../../../../../lib/server/mutationFetcher";
+import {useRouter} from "next/navigation";
+import DateOfVisitForm from "@/app/producer/expert-naft/detail/[uid]/components/date-of-visit-form";
+import {useForm} from "antd/es/form/Form";
+import {useState} from "react";
+import {apiUrl} from "../../../../../../Constants/apiUrl";
+import useGetStep from "../../../../../../hooks/workFlowRequest/useGetStep";
 
 interface PropType {
-  params: { uid: string };
+    params: { uid: string };
 }
 
 interface DataFetchType {
-  choices: Choice[];
-  task: {
-    processId: string;
-    stepId: string;
-    reference_ID: string;
-    group_ID: string;
-    step_Name: string;
-    counting_position: string;
-    userId: number;
-  };
+    choices: Choice[];
+    task: {
+        processId: string;
+        stepId: string;
+        reference_ID: string;
+        group_ID: string;
+        step_Name: string;
+        counting_position: string;
+        userId: number;
+    };
 }
 
+interface SetStep3Type {
+    taskId: string,
+    choiceKey: string,
+    description: string,
+    datePersian1: string,
+    datePersian2: string,
+    datePersian3: string
+}
+
+const apiData = apiUrl.WorkFlowRequest.step03
+
 export default function Home(props: PropType) {
-  const { isLoading, data, mutate } = useSWR<DataFetchType>(
-    "/WorkFlowRequest/GetStep03",
-    (url) =>
-      listFetcher(url, {
-        arg: {
-          taskId: props.params.uid,
-        },
-      })
-  );
-  const { isMutating, trigger } = useSWRMutation(
-    "/WorkFlowRequest/SetStep03",
-    mutationFetcher
-  );
 
-  const [form] = useForm();
-  const router = useRouter();
-  const handleFormSubmit = async (values: any) => {
-    values.taskId = props.params.uid;
-    values.choiceKey = choice;
-    const res = await trigger(values);
-    await mutate();
-    if (res) {
-      setModalVisible(false);
-      form.resetFields();
-      router.push("/producer/expert-naft/list");
+    const [form] = useForm()
+
+    const {isLoading, data} = useGetStep({apiUrl: apiData.get.url, taskId: props.params.uid})
+
+    const {isMutating, trigger} = useSWRMutation(
+        apiData.create.url,
+        mutationFetcher
+    );
+
+    const router = useRouter();
+
+    const [choice, setChoice] = useState<string>("");
+
+    const onFinish = async (values: SetStep3Type) => {
+
+        const data = {
+            ...values,
+            taskId: props.params.uid,
+            choiceKey: choice,
+        }
+
+        const res = await trigger(data)
+
+        if (res)
+            router.push("/producer/expert-naft/list")
+
     }
-  };
 
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const closeModal = () => {
-    setModalVisible(false);
-    form.resetFields();
-  };
-  const [choice, setChoice] = useState("");
-
-  return (
-    <>
-      <div className="box-border w-full p-6">
-        <WorkflowDataViewer loading={isLoading} data={data as any} />
-        {data && <Divider />}
-        <WorkflowRequestBtn
-          onClick={(choiceKey) => {
-            setModalVisible(true);
-            setChoice(choiceKey);
-          }}
-          choices={addIndexToData(data?.choices) as any}
-          nextStepUrl={"/WorkFlowRequest/SetStep03"}
-          taskId={data?.task.stepId as string}
-        />
-        <Modal
-          width={800}
-          title={
-            <div>
-              <div className="text-base mb-2">افزودن دسته بندی جدید</div>
-              <div className="font-normal text-sm">
-                لطفا اطلاعات را وارد نمایید.
-              </div>
+    return (
+        <>
+            <div className="box-border w-full p-6">
+                <DateOfVisitForm form={form} onFinish={onFinish}/>
+                <WorkflowDataViewer loading={isLoading} data={data as any}/>
+                {data && <Divider/>}
+                {
+                    data?.choices &&
+                    !isLoading &&
+                    data?.choices.map((value, index) => <>
+                        <div
+                            style={{height: "fit-content"}}
+                            className="flex justify-center"
+                            key={index}
+                        >
+                            <Button
+                                loading={isMutating}
+                                onClick={() => {
+                                    setChoice(value.choice_Key);
+                                    form.submit()
+                                }}
+                                className="w-full"
+                                type="primary"
+                            >
+                                {value.label}
+                            </Button>
+                        </div>
+                    </>)
+                }
             </div>
-          }
-          open={modalVisible}
-          onCancel={closeModal}
-          footer={[
-            <Row key={"box"} gutter={[16, 16]} className="my-2">
-              <Col xs={24} md={12}>
-                <Button
-                  onClick={() => {
-                    form.submit();
-                  }}
-                  size="large"
-                  className="w-full"
-                  type="primary"
-                  key={"submit"}
-                  loading={isMutating}
-                >
-                  ثبت
-                </Button>
-              </Col>
-              <Col xs={24} md={12}>
-                <Button
-                  size="large"
-                  className="w-full bg-gray-100 text-warmGray-500"
-                  onClick={closeModal}
-                  key={"cancel"}
-                >
-                  انصراف
-                </Button>
-              </Col>
-            </Row>,
-          ]}
-        >
-          <Form
-            onFinish={handleFormSubmit}
-            disabled={isMutating}
-            form={form}
-            layout="vertical"
-          >
-            <Row gutter={[16, 16]}>
-              <Col xs={24} md={12}>
-                <Form.Item
-                  rules={[{ required: true }]}
-                  name="description"
-                  label=" توضیحات کارشناس نفت"
-                >
-                  <Input size="large" placeholder="وارد کنید" />
-                </Form.Item>
-              </Col>
-              <Col xs={24} md={12}>
-                <Form.Item
-                  rules={[{ required: true }]}
-                  name="datePersian"
-                  label="تاریخ بازدید کارشناس نفت"
-                >
-                  <CustomeDatePicker />
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-        </Modal>
-      </div>
-    </>
-  );
+        </>
+    );
 }

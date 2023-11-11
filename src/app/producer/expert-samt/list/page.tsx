e01436d995069b06606b364e9c0763251c37bd6d
madhi-nasirlo <m.nwordpress1223@gmail.com>
2023-11-11 17:59:37 +0330

    ADD:: create step 02

--- src/app/producer/expert-samt/list/page.tsx
+++ src/app/producer/expert-samt/list/page.tsx
@@ -1,32 +1,70 @@
 "use client";
 
-import React, { useState } from "react";
-import useSWR from "swr";
-import DataTable from "./components/data-table";
-import { listFetcher } from "../../../../../lib/server/listFetcher";
-import { addIndexToData } from "../../../../../lib/addIndexToData";
+import React from "react";
+import WorkflowDataTableProvider from "../../../../../components/Workflow/WorkflowDataTable/workflowDataTableProvider";
+import WorkflowDataTable from "../../../../../components/Workflow/WorkflowDataTable";
+import {
+  WorkflowDataTableContextType
+} from "../../../../../components/Workflow/WorkflowDataTable/workflowDataTableContext";
+import {apiUrl} from "../../../../../Constants/apiUrl";
+import {Button, Space} from "antd";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
-  const {
-    data: naft,
-    isLoading,
-    mutate,
-    isValidating,
-  } = useSWR<any>(
-    ["/WorkFlowRequest/GetAllStep03"],
-    ([url, arg]: [url: string, arg: any]) => listFetcher(url)
-  );
+
+  const router = useRouter()
+
+  const initialData: WorkflowDataTableContextType = {
+    apiUrl: apiUrl.WorkFlowRequest.step04.getAll.url,
+    columns: [
+      {
+        title: "ردیف",
+        dataIndex: "Row",
+        key: "1",
+        width: "5%",
+      },
+      {
+        title: "توضیحات کاربر",
+        dataIndex: "userDescription",
+        key: "2",
+      },
+      {
+        title: "تاریخ شروع",
+        dataIndex: "startTimePersian",
+        key: "7",
+      },
+      {
+        title: "تاریخ پایان",
+        dataIndex: "currentStepStartTimePersian",
+        key: "3",
+      },
+
+      {
+        title: "عملیات",
+        key: "عملیات",
+        align: "center",
+        fixed: "right",
+        width: "10%",
+        render: (_, record) => (
+            <Space size="small">
+              <Button
+                  type="link"
+                  className="text-secondary-500 font-bold "
+                  onClick={() => {
+                    router.push("/producer/expert-samt/detail/" + record.taskId);
+                  }}
+              >
+                مشاهده اطلاعات
+              </Button>
+            </Space>
+        ),
+      },
+    ]
+  }
 
   return (
-    <>
-      <div className="box-border w-full mt-8 p-6">
-        <DataTable
-          isValidating={isValidating}
-          mutate={mutate}
-          naft={addIndexToData(naft)}
-          isLoading={isLoading}
-        />
-      </div>
-    </>
+      <WorkflowDataTableProvider initialValue={initialData}>
+        <WorkflowDataTable/>
+      </WorkflowDataTableProvider>
   );
 }

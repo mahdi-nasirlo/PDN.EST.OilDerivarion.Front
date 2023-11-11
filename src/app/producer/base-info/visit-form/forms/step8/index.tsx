82848549196ce1bfbb99292575710929214d1558
morteza-vahdati <mvahdati1382@gmail.com>
2023-11-11 14:30:23 +0330

    Created the visit-form step8 in basic-info

--- src/app/producer/base-info/visit-form/forms/step8/index.tsx
+++ src/app/producer/base-info/visit-form/forms/step8/index.tsx
@@ -1,13 +1,18 @@
 import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon, PencilSquareIcon } from '@heroicons/react/24/outline'
 import { Button, Divider, Form, Typography } from 'antd'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import StepContext from '../../stete-manager/step-context';
 import { useForm } from 'antd/es/form/Form';
+import DisplayButton from './components/display-form/display-button';
+import EditButton from './components/edit-form/edit-button';
+import DisplayForm from './components/display-form/display-form';
+import EditForm from './components/edit-form/edit-form';
 
 export default function Index() {
 
     const processController = useContext(StepContext);
     const [form] = useForm();
+    const [isEditVisible, setIsEditVisible] = useState(true);
 
     return (
         <>
@@ -18,32 +23,30 @@ export default function Index() {
                         تجهیزات اتاق کنترل ( 8 از 8 )
                     </Typography>
                 </div>
-                <div className='flex gap-3 justify-end'>
-                    <Button
-                        className="flex justify-center items-center gap-2"
-                        size="large"
-                        type="primary"
-                        htmlType="submit"
-                        icon={<PencilSquareIcon width={24} height={24} />}
-                    // onClick={showModal}
-                    >
-                        ویرایش
-                    </Button>
-                    <Button
-                        className="bg-gray-50 flex items-center justify-center"
-                        size="large"
-                        type="default"
-                        htmlType="submit"
-                        icon={<ChevronDoubleRightIcon width={24} height={24} />}
-                        onClick={() => processController.dispatch({ type: "PREVIOUS" })}
-                    >
-                        صفحه قبل
-                    </Button>
-                </div>
+                {isEditVisible && <DisplayButton setIsEditVisible={setIsEditVisible} />}
+                {!isEditVisible &&
+                    <EditButton
+                        form={form}
+                        // mutate={mutate}
+                        // isMutating={isMutating}
+                        setIsEditVisible={setIsEditVisible}
+                    />
+                }
             </div >
             <Divider />
-            <Form form={form}>
-                مرحله 8
-            </Form>        </>
+            {isEditVisible &&
+                <Form form={form} layout='vertical' disabled>
+                    <DisplayForm />
+                </Form>}
+            {!isEditVisible &&
+                // <Spin spinning={isMutating || ldProducerLab}>
+                <Form layout="vertical" form={form}
+                // onFinish={onSubmitFinish}
+                >
+                    <EditForm />
+                </Form>
+                // </Spin >
+            }
+        </>
     )
 }

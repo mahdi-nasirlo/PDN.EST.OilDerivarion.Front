571ee7f8eb76f755c675232cddca1d2a52a2f56a
madhi-nasirlo <m.nwordpress1223@gmail.com>
2023-11-11 17:59:06 +0330

    ADD:: create step 01

--- src/app/producer/task/list/page.tsx
+++ src/app/producer/task/list/page.tsx
@@ -1,21 +1,72 @@
 "use client";
 
-import React, {useState} from "react";
-import DataTable from "./components/data-table";
+import React from "react";
+import WorkflowDataTableProvider from "../../../../../components/Workflow/WorkflowDataTable/workflowDataTableProvider";
+import {
+  WorkflowDataTableContextType
+} from "../../../../../components/Workflow/WorkflowDataTable/workflowDataTableContext";
+import WorkflowDataTable from "../../../../../components/Workflow/WorkflowDataTable";
+import {apiUrl} from "../../../../../Constants/apiUrl";
+import {Button, Space} from "antd";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
-  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
 
-  const showModal = () => {
-    setIsEditModalVisible(true);
-  };
+  const router = useRouter()
+
+  const initialValue: WorkflowDataTableContextType = {
+    apiUrl: apiUrl.WorkFlowRequest.step02.getAll.url,
+    columns: [
+      {
+        title: "ردیف",
+        dataIndex: "Row",
+        key: "1",
+        width: "5%",
+      },
+      {
+        title: "توضیحات کاربر",
+        dataIndex: "userDescription",
+        key: "2",
+      },
+      {
+        title: "تاریخ شروع",
+        dataIndex: "startTimePersian",
+        key: "7",
+      },
+      {
+        title: "تاریخ پایان",
+        dataIndex: "currentStepStartTimePersian",
+        key: "3",
+      },
+
+      {
+        title: "عملیات",
+        key: "عملیات",
+        align: "center",
+        fixed: "right",
+        width: "10%",
+        render: (_, record) => (
+            <Space size="small">
+              <Button
+                  type="link"
+                  className="text-secondary-500 font-bold "
+                  onClick={() => {
+                    router.push("/producer/task/detail/" + record.taskId);
+                  }}
+              >
+                مشاهده اطلاعات
+              </Button>
+            </Space>
+        ),
+      },
+    ]
+  }
 
   return (
-    <>
-      <div className="box-border w-full mt-8 p-6">
-        <DataTable
-        />
-      </div>
-    </>
+      <>
+        <WorkflowDataTableProvider initialValue={initialValue}>
+          <WorkflowDataTable/>
+        </WorkflowDataTableProvider>
+      </>
   );
 }

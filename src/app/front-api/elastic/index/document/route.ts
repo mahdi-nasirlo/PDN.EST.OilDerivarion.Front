9b324223bfbbe7588fdd19881f72c575c9d458ed
mahdi <m.nwordpress1223@gmail.com>
2023-11-12 16:12:49 +0330

    test

--- src/app/front-api/elastic/index/document/route.ts
+++ src/app/front-api/elastic/index/document/route.ts
@@ -1,16 +1,17 @@
 import {env} from "../../../../../../lib/env";
+import elasticClient from "../../../../../../lib/logger/elasticClient";
 
 export async function GET() {
 
     try {
-        // const res = await elasticClient.index({
-        //     index: env.ELASTIC_INDEX_NAME,
-        //     id: env.ELASTIC_DOCUMENT_ID,
-        //     document: {}
-        // })
-        // //
-        // return new Response(JSON.stringify(res))
-        return new Response(JSON.stringify(env))
+        const res = await elasticClient.index({
+            index: env.ELASTIC_INDEX_NAME,
+            id: env.ELASTIC_DOCUMENT_ID,
+            document: {}
+        })
+        //
+        return new Response(JSON.stringify(res))
+
     } catch (e: any) {
 
         const data = env.NODE_ENV === "production" ? {

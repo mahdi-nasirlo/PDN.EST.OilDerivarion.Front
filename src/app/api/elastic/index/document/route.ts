617cf8f1d259507adb9ff74c2e00b83f747f3842
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2023-11-12 14:16:02 +0330

    test

--- src/app/api/elastic/index/document/route.ts
+++ src/app/api/elastic/index/document/route.ts
@@ -5,25 +5,25 @@ import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
 
-    const testDocument = {test: "test"}
+    // const testDocument = {test: "test"}
 
-    try {
-        const res = await elasticClient.index({
-            index: env.ELASTIC_INDEX_NAME,
-            id: env.ELASTIC_DOCUMENT_ID,
-            document: testDocument
-        })
+    // try {
+    //     const res = await elasticClient.index({
+    //         index: env.ELASTIC_INDEX_NAME,
+    //         id: env.ELASTIC_DOCUMENT_ID,
+    //         document: testDocument
+    //     })
 
-        return new Response(JSON.stringify(res))
-    } catch (e: any) {
+    //     return new Response(JSON.stringify(res))
+    // } catch (e: any) {
 
-        const data = env.NODE_ENV === "production" ? {
-            success: false,
-            message: e?.meta?.body?.error?.type,
-            statusCode: e?.meta?.statusCode
-        } : e
+    //     const data = env.NODE_ENV === "production" ? {
+    //         success: false,
+    //         message: e?.meta?.body?.error?.type,
+    //         statusCode: e?.meta?.statusCode
+    //     } : e
 
-        return new Response(JSON.stringify(data))
-    }
+    //     return new Response(JSON.stringify(data))
+    // }
 
 }

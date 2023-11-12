22d1e9ecfae687fcaf290c5dc027f0d9eb34ba86
mahdi <m.nwordpress1223@gmail.com>
2023-11-12 15:24:41 +0330

    test

--- src/app/api/elastic/index/document/route.ts
+++ src/app/api/elastic/index/document/route.ts
@@ -1,5 +1,19 @@
 import {NextRequest} from "next/server";
+import {env} from "../../../../../../lib/env";
 
 function GET(request: NextRequest) {
+
+    try {
+
+    } catch (e: any) {
+
+        const data = env.NODE_ENV === "production" ? {
+            success: false,
+            message: e?.meta?.body?.error?.type,
+            statusCode: e?.meta?.statusCode
+        } : e
+
+        return new Response(JSON.stringify(data))
+    }
 
 }

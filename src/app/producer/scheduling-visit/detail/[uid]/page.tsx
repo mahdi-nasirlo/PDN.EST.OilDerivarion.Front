ef511e8bcd5b4c7d8f279884f164318b74532c6a
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2023-11-12 16:48:41 +0330

    added lablecol amd wrappwrcol

--- src/app/producer/scheduling-visit/detail/[uid]/page.tsx
+++ src/app/producer/scheduling-visit/detail/[uid]/page.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { apiUrl } from "../../../../../../Constants/apiUrl";
 import useGetStep from "../../../../../../hooks/workFlowRequest/useGetStep";
 import WorkflowDataViewer from "../../../../../../components/Workflow/WorkflowDataViewer";
@@ -11,85 +11,89 @@ import { useForm } from "antd/es/form/Form";
 import useSWRMutation from "swr/mutation";
 import { mutationFetcher } from "../../../../../../lib/server/mutationFetcher";
 import { useRouter } from "next/navigation";
-import CustomeDatePicker from '../../../../../../components/CustomeDatePicker';
+import CustomeDatePicker from "../../../../../../components/CustomeDatePicker";
 
-const apiData = apiUrl.WorkFlowRequest.step05
+const apiData = apiUrl.WorkFlowRequest.step05;
 
 const Page = (props: { params: { uid: string } }) => {
+  const { data, isLoading } = useGetStep({
+    taskId: props.params.uid,
+    apiUrl: apiData.get.url,
+  });
 
-    const { data, isLoading } = useGetStep({ taskId: props.params.uid, apiUrl: apiData.get.url })
+  const [form] = useForm();
 
-    const [form] = useForm()
+  const [choice, setChoice] = useState<string>();
 
-    const [choice, setChoice] = useState<string>()
+  const router = useRouter();
 
-    const router = useRouter()
+  const handleOnClick = (choice: string) => {
+    setChoice(choice);
 
-    const handleOnClick = (choice: string) => {
+    form.submit();
+  };
 
-        setChoice(choice)
+  const { trigger, isMutating } = useSWRMutation(
+    apiData.create.url,
+    mutationFetcher
+  );
 
-        form.submit()
+  const handleOnFinish = async (values: any) => {
+    const data = {
+      ...values,
+      taskId: props.params.uid,
+      choiceKey: choice,
+    };
 
-    }
+    const res = await trigger(data);
 
-    const { trigger, isMutating } = useSWRMutation(apiData.create.url, mutationFetcher)
+    if (res) router.push("/producer/scheduling-visit/list");
+  };
 
-    const handleOnFinish = async (values: any) => {
-
-        const data = {
-            ...values,
-            taskId: props.params.uid,
-            choiceKey: choice,
-        }
-
-        const res = await trigger(data)
-
-        if (res)
-            router.push("/producer/scheduling-visit/list")
-
-    }
-
-    return (
-        <div className="box-border w-full p-6">
-            <WorkflowDataViewer loading={isLoading} data={data as any} />
-            <Form onFinish={handleOnFinish} form={form}>
-                <Row gutter={[16, 16]}>
-                    <Col xs={24} md={12}>
-                        <Form.Item
-                            rules={[{ required: true }]}
-                            name="datePersian1"
-                            label='تاریخ بازدید نهایی'
-                        >
-                            <CustomeDatePicker />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={[16, 16]}>
-                    <Col xs={24} md={24}>
-                        <Form.Item
-                            name="description"
-                            label="توضیحات"
-                        >
-                            <Input.TextArea
-                                style={{ height: 100, resize: "none" }}
-                                placeholder="وارد کنید"
-                            />
-                        </Form.Item>
-                    </Col>
-                </Row>
-            </Form>
-            {/* <DateOfVisitForm onFinish={handleOnFinish} form={form} /> */}
-            {data && <Divider />}
-            <WorkflowRequestBtn
-                onClick={handleOnClick}
-                choices={data?.choices as any}
-                nextStepUrl={""}
-                taskId={props.params.uid}
-                loading={isMutating}
-            />
-        </div>
-    );
+  return (
+    <div className="box-border w-full p-6">
+      <WorkflowDataViewer loading={isLoading} data={data as any} />
+      <Form onFinish={handleOnFinish} form={form}>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} md={12}>
+            <Form.Item
+              wrapperCol={{ span: 24 }}
+              labelCol={{ span: 24 }}
+              rules={[{ required: true }]}
+              name="datePersian1"
+              label="تاریخ بازدید نهایی"
+            >
+              <CustomeDatePicker />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} md={24}>
+            <Form.Item
+              wrapperCol={{ span: 24 }}
+              labelCol={{ span: 24 }}
+              name="description"
+              label="توضیحات"
+            >
+              <Input.TextArea
+                style={{ height: 100, resize: "none" }}
+                placeholder="وارد کنید"
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+      {/* <DateOfVisitForm onFinish={handleOnFinish} form={form} /> */}
+      {data && <Divider />}
+      <WorkflowRequestBtn
+        onClick={handleOnClick}
+        choices={data?.choices as any}
+        nextStepUrl={""}
+        taskId={props.params.uid}
+        loading={isMutating}
+      />
+    </div>
+  );
 };
 
 export default Page;

d2b5d7174be1bf943f7a9226fa404f04200a9902
mahdi <m.nwordpress1223@gmail.com>
2023-11-18 13:39:50 +0330

    ADD:: link of steps workflow

--- src/app/producer/layout.tsx
+++ src/app/producer/layout.tsx
@@ -2,32 +2,32 @@
 
 import AppLayout from "@/components/layout/layout";
 import React from "react";
-import { Badge, MenuProps } from "antd";
+import {Badge, MenuProps} from "antd";
 import Link from "next/link";
 import {
-  BeakerIcon,
-  DocumentDuplicateIcon,
-  DocumentMagnifyingGlassIcon,
-  HomeIcon,
-  PencilIcon,
-  PhoneIcon,
-  UserCircleIcon,
-  UsersIcon,
-  WrenchScrewdriverIcon,
+    BeakerIcon,
+    DocumentDuplicateIcon,
+    DocumentMagnifyingGlassIcon,
+    HomeIcon,
+    PencilIcon,
+    PhoneIcon,
+    UserCircleIcon,
+    UsersIcon,
+    WrenchScrewdriverIcon,
 } from "@heroicons/react/24/outline";
-import { ListBulletIcon } from "@heroicons/react/24/solid";
-import { getMenuItem } from "@/components/layout/sidebar";
+import {ListBulletIcon} from "@heroicons/react/24/solid";
+import {getMenuItem} from "@/components/layout/sidebar";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <>
-      <AppLayout sidebarItems={items}>{children}</AppLayout>
-    </>
-  );
+    return (
+        <>
+            <AppLayout sidebarItems={items}>{children}</AppLayout>
+        </>
+    );
 }
 
 const MakeRequest = () => (
@@ -82,17 +82,101 @@ const items: MenuProps["items"] = [
         "/producer/step05/list"
       ),
       getMenuItem(
-        <Link href={"/producer/step06/list"}>
-          رویت اعلان ها توسط متقاضی و پرداخت هزینه
-        </Link>,
-        "/producer/step06/list"
+          <Link href={"/producer/step06/list"}>
+              رویت اعلان ها توسط متقاضی و پرداخت هزینه
+          </Link>,
+          "/producer/step06/list"
       ),
-      getMenuItem(
-        <Link href={"/producer/step07/list"}>
-          تولید QR کد و امکان چاپ
-        </Link>,
-        "/producer/step07/list"
-      ),
+        getMenuItem(
+            <Link href={"/producer/step07/list"}>
+                تولید QR کد و امکان چاپ
+            </Link>,
+            "/producer/step07/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step08/list"}>
+                08
+            </Link>,
+            "/producer/step08/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step09/list"}>
+                09
+            </Link>,
+            "/producer/step09/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step10/list"}>
+                10
+            </Link>,
+            "/producer/step10/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step11/list"}>
+                11
+            </Link>,
+            "/producer/step11/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step12/list"}>
+                12
+            </Link>,
+            "/producer/step12/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step13/list"}>
+                13
+            </Link>,
+            "/producer/step13/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step14/list"}>
+                14
+            </Link>,
+            "/producer/step14/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step15/list"}>
+                15
+            </Link>,
+            "/producer/step15/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step16/list"}>
+                16
+            </Link>,
+            "/producer/step16/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step17/list"}>
+                17
+            </Link>,
+            "/producer/step17/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step18/list"}>
+                18
+            </Link>,
+            "/producer/step18/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step19/list"}>
+                19
+            </Link>,
+            "/producer/step19/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step20/list"}>
+                20
+            </Link>,
+            "/producer/step20/list"
+        ),
+        getMenuItem(
+            <Link href={"/producer/step21/list"}>
+                21
+            </Link>,
+            "/producer/step21/list"
+        ),
     ],
     "group"
   ),

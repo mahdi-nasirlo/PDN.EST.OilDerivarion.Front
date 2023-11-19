c2f10331e015a617541a82955b173ba9fadd57ce
mahdi <m.nwordpress1223@gmail.com>
2023-11-19 11:55:18 +0330

    FIX:: error of request

--- lib/server/formBuilderFetcher.ts
+++ lib/server/formBuilderFetcher.ts
@@ -1,8 +1,8 @@
 import reportLog from "../logger/reportLog";
-import { reportLogEnum } from "../logger/reportLogEnum";
+import {reportLogEnum} from "../logger/reportLogEnum";
 import handleError from "./handleError";
-import axios, { AxiosResponse } from "axios";
-import { dataType } from "./listFetcher";
+import axios, {AxiosResponse} from "axios";
+import {dataType} from "./listFetcher";
 
 const FormBuilderFetcher = async (
   url: string,
@@ -10,7 +10,7 @@ const FormBuilderFetcher = async (
 ) => {
   const baseUrlApi = "http://192.168.57.52:1012";
 
-  console.log(`${baseUrlApi}${url}`);
+  console.log(url);
 
   try {
     const res: AxiosResponse = await axios.post(`${baseUrlApi}${url}`, arg);

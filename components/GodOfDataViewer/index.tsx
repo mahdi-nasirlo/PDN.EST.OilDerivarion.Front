baea0f1320c6f382bf04a1e4931e3b2a93ee4948
mahdi <m.nwordpress1223@gmail.com>
2023-12-02 20:41:31 +0330

    ADD:: god of components

---
+++ components/GodOfDataViewer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface PropsType{
+    data: any,
+    loading?: boolean
+}
+
+const Index = (props: {}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Index;

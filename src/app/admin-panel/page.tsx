9c5e07c7bc06ed11f9a16bcc990df0357dcb5537
morteza-vahdati <mvahdati1382@gmail.com>
2023-12-09 16:25:11 +0330

    fixed style div box-border

--- src/app/admin-panel/page.tsx
+++ src/app/admin-panel/page.tsx
@@ -1,18 +1,14 @@
 "use client"
 
 import { Typography } from "antd";
-import GodOfDataViewer from '../../../components/GodOfDataViewer';
 
 export default function Manufacturer() {
 
     return (
-        <div className="box-border w-full mt-8 p-6 text-gray-900">
+        <div className="box-border w-full p-6 text-gray-900">
             <Typography className="text-right text-2xl font-bold">
                 پنل ادمین
             </Typography>
-            <div className="mt-10">
-                <GodOfDataViewer />
-            </div>
         </div>
     );
 }
--- src/app/laboratory-panel/page.tsx
+++ src/app/laboratory-panel/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import {Typography} from 'antd';
+import { Typography } from 'antd';
 import React from 'react'
 
 export default function Page() {
     return (
-        <div className="box-border w-full mt-8 p-6 text-gray-900">
+        <div className="box-border w-full p-6 text-gray-900">
             <Typography className="text-right text-2xl font-bold">
                 پنل آزمایشگاه
             </Typography>

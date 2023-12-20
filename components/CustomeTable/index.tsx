f1a8f0c7f876582c6938bf776008c4e1112236cc
mahdi <m.nwordpress1223@gmail.com>
2023-12-20 12:07:54 +0330

    FIX:: counting after filter

--- components/CustomeTable/index.tsx
+++ components/CustomeTable/index.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Table, TableProps} from "antd";
+import React, {useEffect, useState} from 'react';
+import {Table, TableProps, Typography} from "antd";
 import {addIndexToData} from "../../lib/addIndexToData";
 import getPageRecordNumber from "../../lib/getPageRecordNumber";
 
@@ -16,27 +16,40 @@ const Index = (props: TableProps<any> & RecordeValue) => {
 
     const [startRow, setStartRow] = useState(1)
 
+    const [page, setPage] = useState(1)
+
+    useEffect(() => {
+        if (((props.data?.count || 1) / 5) < page) {
+            setPage(1)
+        }
+    }, [props.data?.count])
+
     return (
         <>
+            <Typography>
+                {JSON.stringify(page)}
+            </Typography>
             <Table
                 {...props}
                 loading={props.isLoading}
-                dataSource={addIndexToData(props.data?.records, "Row", startRow)}
+                dataSource={addIndexToData(props.data?.records, "Row", ((page - 1) * 5) + 1)}
                 className="mt-6"
                 columns={props.columns}
                 pagination={{
                     total: props.data?.count,
                     onChange: async (e: any) => {
-                        setStartRow(getPageRecordNumber(e).fromRecord + 1)
-                        await props.setInitialData((prev: any) => {
+                        setPage(e)
+                        props.setInitialData((prev: any) => {
                             delete prev.fromRecord
                             delete prev.selectRecord
                             return {...getPageRecordNumber(e), ...prev}
                         })
+                        setStartRow(getPageRecordNumber(e).fromRecord + 1)
                     },
                     defaultPageSize: 5,
                     showSizeChanger: false,
                     defaultCurrent: 1,
+                    current: page,
                     style: {
                         display: "flex",
                         flexDirection: "row",

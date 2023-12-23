4516cd6d94b14c3e22249697d794124b3af42e22
morteza-vahdati <mvahdati1382@gmail.com>
2023-12-23 16:35:13 +0330

    test GPS in the map

--- src/app/admin-panel/GPS/gps-devices/location-gps-device/page.tsx
+++ src/app/admin-panel/GPS/gps-devices/location-gps-device/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-
 import React from "react";
 import MapViewer from "../../../../../../components/MapViewer"
 
 export default function Page() {
 
-    return (
+  return (
     <>
       <div className="box-border w-full h-[74vh] p-4">
-          <MapViewer secoandReload={30}/>
+        <MapViewer SecondReload={30} />
       </div>
     </>
   );

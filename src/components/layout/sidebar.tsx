e66d56fa359866647bb97da6a79ad575ea7287e9
mahdi <m.nwordpress1223@gmail.com>
2023-12-24 12:59:52 +0330

    FIX::

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import React, {useState} from "react";
-import type {MenuProps} from "antd";
-import {Drawer, Menu} from "antd";
+import {Drawer, Menu, MenuProps, Skeleton} from "antd";
 import Image from "next/image";
-import {motion} from "framer-motion";
 import {usePathname} from "next/navigation";
+import useSsoGetAllUserAccess from "../../../hooks/sso/useSsoGetAllUserAccess";
+import Link from "next/link";
 
 export default function LayoutSidebar({
                                           menu,
@@ -16,60 +16,58 @@ export default function LayoutSidebar({
     onClose: any;
     open: any;
 }) {
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  const [openKeys, setOpenKeys] = useState<string[]>([]);
+    const [openKeys, setOpenKeys] = useState<string[]>([]);
 
-  const handleMenuOpenChange = (keys: string[]) => {
-    if (keys.length <= 1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys([keys[1]]);
-    }
-  };
+    const handleMenuOpenChange = (keys: string[]) => {
+        if (keys.length <= 1) {
+            setOpenKeys(keys);
+        } else {
+            setOpenKeys([keys[1]]);
+        }
+    };
 
-  const handleMenuItemClick = (item: any) => {
-    if (item.type !== "LgSize") {
-      onClose(); // Close the Drawer
-    }
-  };
+    const handleMenuItemClick = (item: any) => {
+        if (item.type !== "LgSize") {
+            onClose(); // Close the Drawer
+        }
+    };
 
-  const CommonMenu = ({
-    style = {},
-    className = "",
-  }: {
-    style: object;
-    className: string;
-  }) => {
-    return (
-        <Menu
-            style={style}
-            defaultSelectedKeys={[pathname]}
-            selectedKeys={[pathname]}
-            openKeys={openKeys}
-            onOpenChange={handleMenuOpenChange}
-            className={className}
-            mode="inline"
-            items={menu}
-            onClick={handleMenuItemClick}
-        />
-    );
-  };
+    const menus = useSsoGetAllUserAccess()
 
-    // const sidebarVariants = {
-    //     hidden: {
-    //         x: '100%', // Start offscreen to the right
-    //         opacity: 0,
-    //     },
-    //     visible: {
-    //         x: 0, // End at the original position
-    //         opacity: 1,
-    //         transition: {
-    //             // type: 'spring',
-    //             // stiffness: 100,
-    //         },
-    //     },
-    // };
+    const CommonMenu = ({
+                            style = {},
+                            className = "",
+                        }: {
+        style: object;
+        className: string;
+    }) => {
+        return (
+            <Menu
+                style={style}
+                defaultSelectedKeys={[pathname]}
+                selectedKeys={[pathname]}
+                openKeys={openKeys}
+                onOpenChange={handleMenuOpenChange}
+                className={className}
+                mode="inline"
+                onClick={handleMenuItemClick}
+            >
+                {menus.isLoading && Array.from({length: 10}).map((value, index) => (
+                    <Skeleton.Input key={index} size="default" className="w-full my-1"/>
+                ))}
+                {!menus.isLoading && Array.isArray(menus.data) && menus.data?.map((item) => (
+                    <Menu.Item key={item.url}>
+                        <Link href={item.url}>
+                            {item.nameFa}
+                        </Link>
+                    </Menu.Item>
+                ))}
+            </Menu>
+        );
+    };
+
 
     return (
         <>
@@ -89,27 +87,27 @@ export default function LayoutSidebar({
                 {/*    animate={{x: 0, opacity: 1}}*/}
                 {/*    transition={{ease: "easeIn", duration: 0.1}}*/}
                 {/*>*/}
-                    <CommonMenu
-                        style={{
-                            width: "270px",
-                            padding: "0 16px",
-                            paddingTop: "30px",
-                            paddingBottom: "30px",
-                            height: "100%",
-                        }}
-                        className="px-4 overflow-auto hidden lg:block"
-                    />
-                    <Drawer
-                        rootClassName="block lg:hidden"
-                        title="سازمان ملی استاندارد"
-                        placement="right"
-                        width={300}
-                        onClose={onClose}
-                        open={open}
-                        className="boa"
-                    >
-                        <CommonMenu style={{height: "100%"}} className=""/>
-                    </Drawer>
+                <CommonMenu
+                    style={{
+                        width: "270px",
+                        padding: "0 16px",
+                        paddingTop: "30px",
+                        paddingBottom: "30px",
+                        height: "100%",
+                    }}
+                    className="px-4 overflow-auto hidden lg:block"
+                />
+                <Drawer
+                    rootClassName="block lg:hidden"
+                    title="سازمان ملی استاندارد"
+                    placement="right"
+                    width={300}
+                    onClose={onClose}
+                    open={open}
+                    className="boa"
+                >
+                    <CommonMenu style={{height: "100%"}} className=""/>
+                </Drawer>
                 {/*</motion.div>*/}
             </div>
         </>
@@ -119,33 +117,34 @@ export default function LayoutSidebar({
 type MenuItem = Required<MenuProps>["items"][number];
 
 export function getMenuItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: "group",
-  href?: string
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: "group",
+    href?: string
 ): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-    href,
-  } as MenuItem;
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+        href,
+    } as MenuItem;
 }
 
 export const SvgIcon = ({
-  src,
-  width = 16,
-  height = 16,
-  className,
-}: {
-  src: string;
-  width?: number;
-  height?: number;
-  className?: string;
+                            src,
+                            width = 16,
+                            height = 16,
+                            className,
+                        }: {
+    src: string;
+    width?: number;
+    height?: number;
+    className?: string;
 }) => (
-  <Image src={src} width={width} height={height} className={className} alt="" />
+    <Image src={src} width={width} height={height} className={className} alt=""/>
 );
+

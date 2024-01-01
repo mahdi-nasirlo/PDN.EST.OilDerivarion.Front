1559d1bee7e7a0ad295004bcffed4a699cd505ff
morteza-vahdati <mvahdati1382@gmail.com>
2024-01-01 19:09:32 +0330

    fixed columns in steps workflow

--- src/app/producer/step02/detail/[uid]/page.tsx
+++ src/app/producer/step02/detail/[uid]/page.tsx
@@ -1,33 +1,20 @@
 "use client";
 
 import WorkflowRequestBtn from "../../../../../../components/Workflow/WorkflowRequestBtn";
-import {Choice} from "../../../../../../interfaces/requestDetail";
-import {Divider, Typography} from "antd";
-import {useRouter} from "next/navigation";
+import { Choice } from "../../../../../../interfaces/requestDetail";
+import { Divider, Typography } from "antd";
+import { useRouter } from "next/navigation";
 import useGetStep from "../../../../../../hooks/workFlowRequest/useGetStep";
-import {apiUrl} from "../../../../../../Constants/apiUrl";
+import { apiUrl } from "../../../../../../Constants/apiUrl";
 import GodOfDataViewer from "../../../../../../components/GodOfDataViewer";
 
 interface PropType {
     params: { uid: string }
 }
 
-interface DataFetchType {
-    choices: Choice[],
-    task: {
-        processId: string,
-        stepId: string,
-        reference_ID: string,
-        group_ID: string,
-        step_Name: string,
-        counting_position: string,
-        userId: number
-    }
-}
-
 export default function Home(props: PropType) {
 
-    const {isLoading, data} = useGetStep({taskId: props.params.uid, apiUrl: apiUrl.WorkFlowRequest.step02.get.url})
+    const { isLoading, data } = useGetStep({ taskId: props.params.uid, apiUrl: apiUrl.WorkFlowRequest.step02.get.url })
 
     const router = useRouter()
 
@@ -38,15 +25,15 @@ export default function Home(props: PropType) {
                     <div className='flex items-center gap-3'>
                         <Typography className='font-bold'>داده های تجمیعی درخواست</Typography>
                     </div>
-                    <Divider/>
+                    <Divider />
                 </div>
-                <GodOfDataViewer uid={props.params.uid} data={data?.tabs} loading={isLoading}/>
-                {/*<WorkflowDataViewer data={data?.tabs as any} loading={isLoading}/>*/}
-                {data && <Divider/>}
-                <WorkflowRequestBtn onClick={() => router.push("/producer/step02/list")}
-                                    choices={data?.choices as Choice[]}
-                                    nextStepUrl={"/WorkFlowRequest/SetStep02"}
-                                    taskId={props.params.uid}/>
+                <GodOfDataViewer uid={props.params.uid} data={data?.tabs} loading={isLoading} />
+                {data && <Divider />}
+                <WorkflowRequestBtn
+                    onClick={() => router.push("/producer/step02/list")}
+                    choices={data?.choices as Choice[]}
+                    nextStepUrl={"/WorkFlowRequest/SetStep02"}
+                    taskId={props.params.uid} />
             </div>
         </>
     );

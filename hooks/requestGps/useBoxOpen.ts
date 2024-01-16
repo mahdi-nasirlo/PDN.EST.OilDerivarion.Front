6105c3fe86927f982fabb457df5fade56d12f1ea
mahdi <m.nwordpress1223@gmail.com>
2024-01-16 14:13:41 +0330

    FIX:: laboratory edit modal

--- hooks/requestGps/useBoxOpen.ts
+++ hooks/requestGps/useBoxOpen.ts
@@ -1,6 +1,5 @@
 import {apiUrl} from "../../Constants/apiUrl";
 import useSWRMutation from "swr/mutation";
-import {notification} from "antd";
 import customFetch from "../../lib/server/customeFetcher";
 import getUrlWithParams from "../../utils/getUrlWithParams";
 
@@ -14,13 +13,7 @@ const fetcher = async (url: string, arg: {arg: any}) => {
     
     const res = await customFetch({url: {absolute: true, path: getUrlWithParams(apiUrl.MapViewer.boxOpen.url,  {code: 1234, device: "C8A4E7DB-5783-4CEB-8DF0-C0EC1BF0C5DA"}) }})
 
-    const json = await res.data
-
-    console.log(json)
-
-    notification.success({message: "دستور باز شدن درب ارسال شد"})
-
-    return json
+    return await res.data
 
 
 }

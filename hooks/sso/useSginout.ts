f329afc8d4431999b34b7d8d2bf2f6a8133f04be
mahdi <m.nwordpress1223@gmail.com>
2024-01-16 11:20:32 +0330

    FIX:: logout

--- hooks/sso/useSginout.ts
+++ hooks/sso/useSginout.ts
@@ -1,6 +1,12 @@
-import {mutationFetcher} from "../../lib/server/mutationFetcher";
 import useSWRMutation from "swr/mutation";
+import customeFetcher from "../../lib/server/customeFetcher";
 
-const useSignOut = () => useSWRMutation("/Sso/Logout", mutationFetcher);
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
+
+
+const useSignOut = () => useSWRMutation("/Sso/Logout", () => customeFetcher({
+    url: {path: "/Sso/Logout"},
+    method: "GET"
+}))
 
 export default useSignOut;

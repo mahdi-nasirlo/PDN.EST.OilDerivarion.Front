a26b1634b2e1b774f6aeba4fd62e14ef3abe032c
mahdi <m.nwordpress1223@gmail.com>
2024-01-23 12:42:28 +0330

    FIX:: api url

--- src/utils/base-axois.ts
+++ src/utils/base-axois.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 
 const baseAxois: AxiosInstance = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/`,
 });
 
 export default baseAxois;

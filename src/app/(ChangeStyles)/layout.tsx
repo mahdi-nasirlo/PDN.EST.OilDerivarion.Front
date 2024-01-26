46b2cf58dd615ca2bbf6137f28ecd73f421f72dc
morteza-vahdati <mvahdati1382@gmail.com>
2024-01-26 15:35:26 +0330

    added role-determination page

---
+++ src/app/(ChangeStyles)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RoleDetermination from './role-determination/page';
+
+function Layout() {
+    return (
+        <div>
+            <RoleDetermination />
+        </div>
+    )
+}
+
+export default Layout

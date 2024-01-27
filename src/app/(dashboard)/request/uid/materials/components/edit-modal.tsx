7a86ba270a19b640d392379c3b82074bdf483c1f
morteza-vahdati <mvahdati1382@gmail.com>
2024-01-27 14:33:06 +0330

    fixed material table & modals in request

--- src/app/(dashboard)/request/uid/materials/components/edit-modal.tsx
+++ src/app/(dashboard)/request/uid/materials/components/edit-modal.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import MaterialForm from './material-form'
+import { Button, Col, Modal, Row } from 'antd'
 
-export default function EditModal() {
+export default function EditModal({ editModal, setEditModal }: any) {
+
+    const handleConfirmEdit = () => setEditModal(false)
+
+    const handleCancelEdit = () => setEditModal(false)
+
     return (
-        <div>
-            EditModal
+        <Modal
+            width={800}
+            title="ویرایش مواد اولیه"
+            open={editModal}
+            onOk={handleConfirmEdit}
+            onCancel={handleCancelEdit}
+            footer={[
+                <Row key={"box"} gutter={[16, 16]} className="my-2">
+                    <Col xs={12} md={12}>
+                        <Button
+                            size="large"
+                            className="w-full"
+                            type="primary"
+                            onClick={handleConfirmEdit}
+                            key={"submit"}
+                        >
+                            ثبت
+                        </Button>
+                    </Col>
+                    <Col xs={12} md={12}>
+                        <Button
+                            size="large"
+                            className="w-full bg-gray-100 text-warmGray-500"
+                            onClick={handleCancelEdit}
+                            key={"cancel"}
+                        >
+                            انصراف
+                        </Button>
+                    </Col>
+                </Row>,
+            ]}
+        >
             <MaterialForm />
-        </div>
+        </Modal>
+
     )
 }

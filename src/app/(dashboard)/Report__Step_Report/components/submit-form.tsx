0b000cf5a0cccc80c7a1601066db60f83d062649
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2024-01-28 10:49:24 +0330

    fixed the title of the transfer opration

--- src/app/(dashboard)/Report__Step_Report/components/submit-form.tsx
+++ src/app/(dashboard)/Report__Step_Report/components/submit-form.tsx
@@ -37,7 +37,7 @@ export default function SubmitForm() {
                 width: "100%",
                 height: 400,
               }}
-              operations={["به راست", "به چپ "]}
+              operations={["اضافه شود", "حذف شود"]}
               onChange={handleOnChange}
               render={(item) => <div className="text-right">{item.title}</div>}
             />

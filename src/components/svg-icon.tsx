95545ccef4cf07ded5aa25a6d07f8d1dc22ab6b8
morteza-vahdati <mvahdati1382@gmail.com>
2024-01-28 17:24:35 +0330

    Added Level icon in header

--- src/components/svg-icon.tsx
+++ src/components/svg-icon.tsx
@@ -1,6 +1,12 @@
-import Image, {ImageProps} from "next/image";
+import Image, { ImageProps } from "next/image";
 import React from "react";
 
 export const SvgIcon = (props: { width?: number, height?: number } & ImageProps) => (
-    <Image {...props} alt={props.alt ?? "icon"} height={props.width ?? 16} width={props.width ?? 16}/>
+    <Image
+        {...props}
+        alt={props.alt ?? "icon"}
+        className={props.className}
+        height={props.width ?? 16}
+        width={props.width ?? 16}
+    />
 );
--- src/layout/hooks/use-header-dropwdown.ts
+++ src/layout/hooks/use-header-dropwdown.ts
@@ -1,24 +1,23 @@
-import { useGetUserInfo } from "@/hooks/sso/use-get-user-info"
-import { useLogout } from "@/hooks/sso/use-logout"
-import { useState } from "react"
+import { useGetUserInfo } from "@/hooks/sso/use-get-user-info";
+import { useLogout } from "@/hooks/sso/use-logout";
+import { useState } from "react";
 
 const useHeaderDropdown = () => {
+  const [openConfirmExitModal, setOpenConfirmExitModal] = useState(false);
 
-    const [openConfirmExitModal, setOpenConfirmExitModal] = useState(false)
+  const userGetInfo = useGetUserInfo();
 
-    const userInfo = useGetUserInfo()
+  const logout = useLogout();
 
-    const logout = useLogout()
+  return {
+    userGetInfo,
+    logout,
+    confirmExitModal: {
+      isOpen: openConfirmExitModal,
+      open: () => setOpenConfirmExitModal(true),
+      close: () => setOpenConfirmExitModal(false),
+    },
+  };
+};
 
-    return {
-        userInfo,
-        logout,
-        confirmExitModal: {
-            isOpen: openConfirmExitModal,
-            open: () => setOpenConfirmExitModal(true),
-            close: () => setOpenConfirmExitModal(false)
-        }
-    }
-}
-
-export {useHeaderDropdown}
+export { useHeaderDropdown };

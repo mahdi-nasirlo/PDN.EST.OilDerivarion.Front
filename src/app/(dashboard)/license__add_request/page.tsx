5f2bb0b4ed2ebc112b5b94ccb20e2be3bd810d25
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2024-02-04 19:27:34 +0330

    added the breadcrum to the

--- src/app/(dashboard)/license__add_request/page.tsx
+++ src/app/(dashboard)/license__add_request/page.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import SubmitForm from "./components/submit-form";
 import DataTable from "./components/data-table";
 import { Card } from "@/components/card";
+import Breadcrumb from "@/components/breadcrumb";
+import { BeakerIcon } from "@heroicons/react/24/outline";
+import { ClipboardDocumentCheckIcon } from "@heroicons/react/24/solid";
 
 const page = () => {
   return (
     <div>
+      <Breadcrumb
+        titleIcon={<ClipboardDocumentCheckIcon className="w-8" />}
+        pages={[{ label: "خانه", path: "/" }]}
+        currentPage={"ثبت مجوز تولید کننده"}
+      />
       <Card>
         <SubmitForm />
       </Card>

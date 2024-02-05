c1ca6a3bef1c649f042b700f4535b04677ecd050
mahdi <m.nwordpress1223@gmail.com>
2024-02-05 19:47:05 +0330

    Test::

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,7 @@
 "use client"
 
 import {useAuth} from '@/hooks/use-auth'
-import {Button, Spin} from 'antd';
-import {Result} from 'antd/lib';
 import React from 'react'
-import Link from "next/link";
 
 interface PropsType {
     searchParams: {
@@ -17,29 +14,30 @@ export default function Page(props: PropsType) {
 
     const { checkToken, getToken } = useAuth()
 
-    return (
-        <Result
-            title="در حال انتقال به صفحه هستید"
-            className="w-full h-[100vh]"
-            status={
-                checkToken?.isLoading || getToken.isLoading ?
-                    "info" :
-                    (checkToken.data?.success || getToken.data?.success ? "success" : "error")
-            }
-            extra={
-                <>
-                    <div className="flex justify-center items-center flex-col">
-                        <Spin size='large' spinning={checkToken.isLoading || getToken.isLoading}/>
-                        {
-                            (!checkToken.isLoading && !getToken.isLoading) &&
-                            (!checkToken.data?.success || !getToken.data?.success) &&
-                            <Link className="mt-6" href={"/login"}>
-                                <Button type="primary">تلاش مجدد</Button>
-                            </Link>
-                        }
-                    </div>
-                </>
-            }
-        />
-    )
+    return <></>
+    // return (
+    //     <Result
+    //         title="در حال انتقال به صفحه هستید"
+    //         className="w-full h-[100vh]"
+    //         status={
+    //             checkToken?.isLoading || getToken.isLoading ?
+    //                 "info" :
+    //                 (checkToken.data?.success || getToken.data?.success ? "success" : "error")
+    //         }
+    //         extra={
+    //             <>
+    //                 <div className="flex justify-center items-center flex-col">
+    //                     <Spin size='large' spinning={checkToken.isLoading || getToken.isLoading}/>
+    //                     {
+    //                         (!checkToken.isLoading && !getToken.isLoading) &&
+    //                         (!checkToken.data?.success || !getToken.data?.success) &&
+    //                         <Link className="mt-6" href={"/login"}>
+    //                             <Button type="primary">تلاش مجدد</Button>
+    //                         </Link>
+    //                     }
+    //                 </div>
+    //             </>
+    //         }
+    //     />
+    // )
 }

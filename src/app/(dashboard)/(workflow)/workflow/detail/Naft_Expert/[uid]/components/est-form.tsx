3e5bc683369c516d1d15c80eaa9305476e664fcd
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2024-02-06 08:36:13 +0330

    added the page of the all times

--- src/app/(dashboard)/(workflow)/workflow/detail/Naft_Expert/[uid]/components/est-form.tsx
+++ src/app/(dashboard)/(workflow)/workflow/detail/Naft_Expert/[uid]/components/est-form.tsx
@@ -1,22 +1,25 @@
-import {Divider, Form, Typography} from "antd";
+import { Divider, Form, Typography } from "antd";
 import React from "react";
-import {FormTime} from "@/app/(dashboard)/(workflow)/workflow/detail/Naft_Expert/[uid]/components/form-time";
+import { FormTime } from "@/app/(dashboard)/(workflow)/workflow/detail/Naft_Expert/[uid]/components/form-time";
 
-export const EstForm = ({disable}: { disable: boolean }) => {
-    return (
-        <>
-            <div className="mb-5">
-                <Typography className="text-right text-[16px] font-bold text-orange-300">
-                    نماینده استاندارد
-                </Typography>
-            </div>
-            <Form
-                disabled={disable}
-                layout="vertical"
-            >
-                <FormTime disable={disable}/>
-            </Form>
-            <Divider/>
-        </>
-    );
+export const EstForm = ({
+  disable,
+  uid,
+}: {
+  disable: boolean;
+  uid?: string;
+}) => {
+  return (
+    <>
+      <div className="mb-5">
+        <Typography className="text-right text-[16px] font-bold text-orange-300">
+          نماینده استاندارد
+        </Typography>
+      </div>
+      <Form disabled={disable} layout="vertical">
+        <FormTime disable={disable} />
+      </Form>
+      <Divider />
+    </>
+  );
 };

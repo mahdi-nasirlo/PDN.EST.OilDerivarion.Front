7b06adeaccdadb5db3f5ad8ead31b506597e9f10
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2024-02-06 15:05:35 +0330

    fixed the bugs

--- src/app/(dashboard)/(workflow)/workflow/detail/Samt_Expert/[uid]/components/samt-form.tsx
+++ src/app/(dashboard)/(workflow)/workflow/detail/Samt_Expert/[uid]/components/samt-form.tsx
@@ -1,9 +1,10 @@
-import { Divider, Form, Typography } from "antd";
+import { Button, Col, Divider, Form, Input, Row, Typography } from "antd";
 import React, { useEffect } from "react";
 import { FormTime } from "@/app/(dashboard)/(workflow)/workflow/detail/Naft_Expert/[uid]/components/form-time";
 import useUiTimeSchedule from "../hook/use-ui-time-schedule";
 import { RequestPackageApi } from "constance/request-package";
 import { useValidation } from "@/hooks/use-validation";
+import CustomDatePicker from "@/components/custome-date-picker";
 
 export const SamtForm = ({
   disable,
@@ -33,7 +34,69 @@ export const SamtForm = ({
         onFinish={handleSubmit}
         layout="vertical"
       >
-        <FormTime disable={disable} />
+        <Row gutter={[16, 16]}>
+          <Col xs={24} md={8}>
+            <Form.Item
+              required={false}
+              rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
+              name="samt_1"
+              label="زمان بازدید احتمالی اول"
+            >
+              <CustomDatePicker />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={8}>
+            <Form.Item
+              required={false}
+              rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
+              name="samt_2"
+              label="زمان بازدید احتمالی دوم"
+            >
+              <CustomDatePicker />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={8}>
+            <Form.Item
+              required={false}
+              rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
+              name="samt_3"
+              label="زمان بازدید احتمالی سوم"
+            >
+              <CustomDatePicker />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} md={24}>
+            <Form.Item
+              required={false}
+              rules={[{ required: true, message: "لطفا مقدار را وارد کنید" }]}
+              name="samt_description"
+              label="توضیحات"
+            >
+              <Input.TextArea
+                style={{ height: 100, resize: "none" }}
+                placeholder="وارد کنید"
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        {!disable && (
+          <Row gutter={[32, 0]}>
+            <Col xs={24} md={24}>
+              <Button
+                disabled={disable}
+                className="w-full"
+                size="large"
+                type={disable ? "default" : "primary"}
+                htmlType="submit"
+              >
+                ثبت
+              </Button>
+            </Col>
+          </Row>
+        )}
+        {/* <FormTime disable={disable} /> */}
       </Form>
       <Divider />
     </>

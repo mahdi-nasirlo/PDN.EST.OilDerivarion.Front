f70353408b64d115168f331463574ed14bc68ed1
morteza-vahdati <mvahdati1382@gmail.com>
2024-02-06 06:40:54 +0330

    fixed again side-bar menu item

--- src/layout/side-bar.tsx
+++ src/layout/side-bar.tsx
@@ -2,94 +2,93 @@ import { Menu, MenuProps } from "antd/lib";
 import React from "react";
 import { useControlSidebar } from "./hooks/use-control-sidebar";
 import { Drawer, Skeleton } from "antd";
+import Link from "next/link";
 
-export default function SideBar({
-  open,
-  setOpen,
-}: {
-  open: boolean;
-  setOpen: (arg: boolean) => void;
-}) {
-  const { userAccess, handleMenuOpenChange, handleMenuItemClick, pathname } =
-    useControlSidebar();
+interface TProps {
+    open: boolean;
+    setOpen: (arg: boolean) => void;
+}
 
-  const CommonMenu = (props: MenuProps) => (
-    <Menu
-      {...props}
-      mode="inline"
-      selectedKeys={[pathname]}
-      defaultSelectedKeys={[pathname]}
-      onClick={handleMenuItemClick}
-      onOpenChange={handleMenuOpenChange}
-      items={userAccess.data}
+export default function SideBar({ open, setOpen }: TProps) {
+
+    const { userAccess, handleMenuOpenChange, handleMenuItemClick, pathname } = useControlSidebar()
+
+    const CommonMenu = (props: MenuProps) => <Menu
+        {...props}
+        mode='inline'
+        selectedKeys={[pathname]}
+        defaultSelectedKeys={[pathname]}
+        onClick={handleMenuItemClick}
+        onOpenChange={handleMenuOpenChange}
+    // items={userAccess.data}
     >
-      <div className="flex flex-col gap-4 sidebar-Skeleton">
-        <Skeleton active />
-        <Skeleton.Input className="w-full" active size="small" />
-        <Skeleton.Input active className="w-2/3" size="small" />
-        <Skeleton.Input active className="w-1/2" size="small" />
-        <Skeleton active />
-        <Skeleton.Input
-          className="w-full"
-          style={{ marginTop: "8px" }}
-          active
-          size="small"
-        />
-        <Skeleton.Input className="w-full" active size="small" />
-        <Skeleton.Input active className="w-1/2" size="small" />
-        <Skeleton.Input active className="w-1/2" size="small" />
-        <Skeleton.Input
-          className="w-full"
-          style={{ marginTop: "8px" }}
-          active
-          size="small"
-        />
-        <Skeleton.Input active className="w-1/2" size="small" />
-        <Skeleton.Input className="w-full" active size="small" />
-      </div>
-    </Menu>
-  );
+        {userAccess.data ? (
+            userAccess.data.map(item => (
+                <Menu.Item key={item.key} className="custom-menu-item">
+                    <Link href={item.key}>
+                        {item.label}
+                    </Link>
+                </Menu.Item>
+            ))
+        )
+            : (
+                <div className="flex flex-col gap-4 sidebar-Skeleton">
+                    <Skeleton active />
+                    <Skeleton.Input className="w-full" active size="small" />
+                    <Skeleton.Input active className="w-2/3" size="small" />
+                    <Skeleton.Input active className="w-1/2" size="small" />
+                    <Skeleton active />
+                    <Skeleton.Input className="w-full" style={{ marginTop: "8px" }} active size="small" />
+                    <Skeleton.Input className="w-full" active size="small" />
+                    <Skeleton.Input active className="w-1/2" size="small" />
+                    <Skeleton.Input active className="w-1/2" size="small" />
+                    <Skeleton.Input className="w-full" style={{ marginTop: "8px" }} active size="small" />
+                    <Skeleton.Input active className="w-1/2" size="small" />
+                    <Skeleton.Input className="w-full" active size="small" />
+                </div>
+            )}
+    </Menu >
 
-  return (
-    <div
-      className="bg-white border-e-0 lg:border-e-[1px] fixed bottom-0 top-[97] left-auto right-0 z-50"
-      style={{
-        position: "fixed",
-        bottom: 0,
-        top: 97,
-        left: "auto",
-        right: 0,
-        zIndex: 99,
-      }}
-    >
-      <Drawer
-        className="mobile-drawer"
-        open={open}
-        onClose={() => setOpen(false)}
-      >
-        <CommonMenu
-          className="px-4 overflow-auto w-full"
-          style={{
-            width: "275px",
-            padding: "0 16px",
-            paddingTop: "30px",
-            paddingBottom: "30px",
-            height: "100%",
-            backgroundColor: "#1C2537",
-          }}
-        />
-      </Drawer>
-      <CommonMenu
-        className="px-4 overflow-auto hidden lg:block"
-        style={{
-          width: "275px",
-          padding: "0 16px",
-          paddingTop: "30px",
-          paddingBottom: "30px",
-          height: "100%",
-          backgroundColor: "#1C2537",
-        }}
-      />
-    </div>
-  );
+    return (
+        <div
+            className="bg-white border-e-0 lg:border-e-[1px] fixed bottom-0 top-[97] left-auto right-0 z-50"
+            style={{
+                position: "fixed",
+                bottom: 0,
+                top: 97,
+                left: "auto",
+                right: 0,
+                zIndex: 99,
+            }}
+        >
+            <Drawer
+                className="mobile-drawer"
+                open={open}
+                onClose={() => setOpen(false)}
+            >
+                <CommonMenu
+                    className="px-4 overflow-auto w-full"
+                    style={{
+                        width: "275px",
+                        padding: "0 16px",
+                        paddingTop: "30px",
+                        paddingBottom: "30px",
+                        height: "100%",
+                        backgroundColor: "#1C2537",
+                    }}
+                />
+            </Drawer>
+            <CommonMenu
+                className="px-4 overflow-auto hidden lg:block"
+                style={{
+                    width: "275px",
+                    padding: "0 16px",
+                    paddingTop: "30px",
+                    paddingBottom: "30px",
+                    height: "100%",
+                    backgroundColor: "#1C2537",
+                }}
+            />
+        </div>
+    );
 }

cb7d2051d51092abda8f3dc513c8707b31c08650
mahdi <m.nwordpress1223@gmail.com>
2024-02-07 18:11:05 +0330

    Add:: state select

---
+++ src/components/fields/state-select-field.tsx
@@ -0,0 +1,21 @@
+import {SelectProps} from "antd/lib";
+import useGetAllState from "@/hooks/basic/use-get-all-state";
+import {Select} from "antd";
+
+
+const StateSelectField = (props: SelectProps) => {
+
+    const materials = useGetAllState()
+
+    return <Select
+        {...props}
+        showSearch
+        options={materials.treeData}
+        loading={materials.isLoading}
+        size="large"
+        placeholder="انتخاب نمایید"
+        tokenSeparators={[","]}
+    />
+}
+
+export {StateSelectField}

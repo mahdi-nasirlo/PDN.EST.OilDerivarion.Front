b9372760a36e17082936916d02aff864828c280b
mahdi <m.nwordpress1223@gmail.com>
2024-02-07 14:11:13 +0330

    Add:: alert for first user

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 
+import {useGetUserAccess} from "@/hooks/sso/use-get-user-access";
+import {Alert} from "antd";
+import {Card} from "@/components/card";
+
 export default function Page() {
 
+  const pages = useGetUserAccess()
 
-  return;
+  return <Card>
+    {(!Array.isArray(pages.data) || pages.data.length == 0) &&
+        <Alert type="warning" className="font-bold text-md"
+               message="اشخاص حقیقی فاقد شرکت قادر به ثبت نام و استفاده از سامانه نمی باشد"/>}
+  </Card>;
 }

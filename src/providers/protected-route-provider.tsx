1f47ed394b965d4554d8cf9245deb1ac04b6ec7d
mahdi <m.nwordpress1223@gmail.com>
2024-02-08 14:46:50 +0330

    Fix::

--- src/providers/protected-route-provider.tsx
+++ src/providers/protected-route-provider.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useGetUserAccess} from "@/hooks/sso/use-get-user-access";
 import {usePathname, useRouter} from "next/navigation";
-import {notification, Spin} from "antd/lib";
+import {notification} from "antd/lib";
 
 const ProtectedRouteProvider = ({children}: { children: React.ReactNode }) => {
 
@@ -13,7 +13,7 @@ const ProtectedRouteProvider = ({children}: { children: React.ReactNode }) => {
 
     const accessPages = useGetUserAccess()
 
-    const [access, setAccess] = useState(false)
+    // const [access, setAccess] = useState(false)
 
     useEffect(() => {
 
@@ -26,17 +26,18 @@ const ProtectedRouteProvider = ({children}: { children: React.ReactNode }) => {
                 notification.error({message: "access denise"})
             }
 
-            setAccess(true)
+            // setAccess(true)
 
         }
 
     }, [accessPages.data, accessPages.isFetching, pathname])
 
-    return access ? children : <div
-        className="w-full flex rounded-lg bg-no-repeat bg-cover bg-bottom "
-    >
-        <Spin className="w-full h-full"/>
-    </div>
+    return children
+    // return access ? children : <div
+    //     className="w-full flex rounded-lg bg-no-repeat bg-cover bg-bottom "
+    // >
+    //     <Spin className="w-full h-full"/>
+    // </div>
 };
 
 export default ProtectedRouteProvider;

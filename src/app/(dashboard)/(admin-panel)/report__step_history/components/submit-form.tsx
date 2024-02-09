bd40538890a5e0bb322f2edaa452dd8112cd4acc
morteza-vahdati <mvahdati1382@gmail.com>
2024-02-09 14:53:25 +0330

    fixed Transfer search in columns pages

--- src/app/(dashboard)/(admin-panel)/report__step_history/components/submit-form.tsx
+++ src/app/(dashboard)/(admin-panel)/report__step_history/components/submit-form.tsx
@@ -6,32 +6,41 @@ import { useControlTransfer } from "../hook/use-control-transfer";
 import { Transfer } from "antd/lib";
 
 export default function SubmitForm() {
-  const { steps, setStep, dataSource, registeredReport, handleOnChange } =
-    useControlTransfer();
+
+  const {
+    steps,
+    setStep,
+    dataSource,
+    registeredReport,
+    handleOnChange
+  } = useControlTransfer();
 
   return (
     <>
       <Row gutter={[16, 16]}>
-        <Col xs={24} md={24}>
-          <Typography className="flex p-2">
-            نقش
-          </Typography>
-          <Select
-            className="w-full"
-            showSearch
-            loading={steps.isLoading}
-            options={steps.data}
-            onChange={(value) => setStep(value)}
-            fieldNames={steps.apiData.fieldNames}
-            size="large"
-            placeholder="انتخاب کنید"
-          />
-        </Col>
+        <Typography className="flex px-2">
+          نقش
+        </Typography>
+        <Select
+          className="w-full"
+          showSearch
+          loading={steps.isLoading}
+          options={steps.data}
+          onChange={(value) => setStep(value)}
+          fieldNames={steps.apiData.fieldNames}
+          size="large"
+          placeholder="انتخاب کنید"
+        />
       </Row>
       <Row gutter={[16, 16]}>
         <Col xs={24} md={24}>
           <div className="w-full mt-8">
             <Transfer
+              filterOption={(inputValue, option) =>
+                option?.title
+                  ? option?.title.toLowerCase().includes(inputValue.toLowerCase())
+                  : false
+              }
               operationStyle={{ gap: 20 }}
               titles={["گزارشات انتخاب نشده", "گزارشات انتخاب شده"]}
               targetKeys={registeredReport.targetKeys}
--- src/app/(dashboard)/(admin-panel)/report__step_report/components/submit-form.tsx
+++ src/app/(dashboard)/(admin-panel)/report__step_report/components/submit-form.tsx
@@ -1,37 +1,45 @@
-"use client";
-
-import { Col, Row, Select, Typography } from "antd";
 import React from "react";
+import { Col, Row, Select, Typography } from "antd";
+import Transfer from "antd/lib/transfer";
 import { useControlTransfer } from "../hook/use-control-transfer";
-import { Transfer } from "antd/lib";
 
 export default function SubmitForm() {
-  const { steps, setStep, dataSource, registeredReport, handleOnChange } =
-    useControlTransfer();
+
+  const {
+    steps,
+    setStep,
+    dataSource,
+    registeredReport,
+    handleOnChange
+  } = useControlTransfer();
+
 
   return (
     <>
       <Row gutter={[16, 16]}>
-        <Col xs={24} md={24}>
-          <Typography className="flex p-2">
-            مرحله
-          </Typography>
-          <Select
-            className="w-full"
-            showSearch
-            loading={steps.isLoading}
-            options={steps.data}
-            onChange={(value) => setStep(value)}
-            fieldNames={steps.apiData.fieldNames}
-            size="large"
-            placeholder="انتخاب کنید"
-          />
-        </Col>
-      </Row>
+        <Typography className="flex px-2">
+          مرحله
+        </Typography>
+        <Select
+          className="w-full"
+          showSearch
+          loading={steps.isLoading}
+          options={steps.data}
+          onChange={(value) => setStep(value)}
+          fieldNames={steps.apiData.fieldNames}
+          size="large"
+          placeholder="انتخاب کنید"
+        />
+      </Row >
       <Row gutter={[16, 16]}>
         <Col xs={24} md={24}>
           <div className="w-full mt-8">
             <Transfer
+              filterOption={(inputValue, option) =>
+                option?.title
+                  ? option?.title.toLowerCase().includes(inputValue.toLowerCase())
+                  : false
+              }
               operationStyle={{ gap: 20 }}
               titles={["گزارشات انتخاب نشده", "گزارشات انتخاب شده"]}
               targetKeys={registeredReport.targetKeys}
@@ -39,7 +47,7 @@ export default function SubmitForm() {
               showSearch
               listStyle={{
                 width: "100%",
-                height: 400,
+                height: 400
               }}
               operations={["به چپ", "به راست"]}
               onChange={handleOnChange}

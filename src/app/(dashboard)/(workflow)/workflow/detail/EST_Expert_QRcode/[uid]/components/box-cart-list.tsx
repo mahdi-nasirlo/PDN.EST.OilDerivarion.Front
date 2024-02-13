88199ef9eb4fe91ad2b3d2dc3669d57a3bf77537
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2024-02-13 13:25:53 +0330

    fix

--- src/app/(dashboard)/(workflow)/workflow/detail/EST_Expert_QRcode/[uid]/components/box-cart-list.tsx
+++ src/app/(dashboard)/(workflow)/workflow/detail/EST_Expert_QRcode/[uid]/components/box-cart-list.tsx
@@ -1,181 +1,217 @@
-import React, {useState} from 'react';
-import {Button, Col, Form, Popover, Row, Select, Typography} from "antd/lib";
-import {motion} from "framer-motion";
-import {Card} from "@/components/card";
-import {PlusSmallIcon} from "@heroicons/react/24/outline";
-import useUiWorkflowSampleAdd
-    from "@/app/(dashboard)/(workflow)/workflow/detail/EST_Expert_QRcode/[uid]/hook/use-ui-workflow-sample-add";
+import React, { useState } from "react";
+import { Button, Col, Form, Popover, Row, Select, Typography } from "antd/lib";
+import { motion } from "framer-motion";
+import { Card } from "@/components/card";
+import { PlusSmallIcon } from "@heroicons/react/24/outline";
+import useUiWorkflowSampleAdd from "@/app/(dashboard)/(workflow)/workflow/detail/EST_Expert_QRcode/[uid]/hook/use-ui-workflow-sample-add";
 import useBoxSampleGetAvailableList from "@/hooks/request-package/use-box-sample-get-available-list";
 import useBoxSampleAdd from "@/hooks/request-package/use-box-sample-add";
-import {errorMessage} from "../../../../../../../../constance/error-message";
-import {RequestPackageApi} from "../../../../../../../../constance/request-package";
-import {z} from "zod";
+import { errorMessage } from "../../../../../../../../constance/error-message";
+import { RequestPackageApi } from "../../../../../../../../constance/request-package";
+import { z } from "zod";
 
-const BoxCartList = ({package_UID}: { package_UID: string }) => {
+const BoxCartList = ({ package_UID }: { package_UID: string }) => {
+  const { boxList, deleteSample } = useUiWorkflowSampleAdd({ package_UID });
 
-    const {boxList, deleteSample} = useUiWorkflowSampleAdd({package_UID})
+  const renderCircles = (
+    item: z.infer<typeof RequestPackageApi.BoxList.item>,
+    cardIndex: number = 0
+  ) => {
+    const views: React.ReactNode[] = [];
 
-    const renderCircles = (item: z.infer<typeof RequestPackageApi.BoxList.item>, cardIndex: number = 0) => {
-
-        const views: React.ReactNode[] = []
-
-        if (Array.isArray(item?.samples)) {
-
-            item.samples?.map((sample, index) => views.push(
-                <Popover title="حذف آیتم" content={<>
-                    <Typography>از حذف ماده اولیه {sample.name} مطمئن هستید؟</Typography>
-                    <div className="flex justify-end">
-                        <Button danger className="mt-3 border-red-500 hover:border-red-500"
-                                loading={deleteSample.isPending}
-                                disabled={deleteSample.isPending}
-                                onClick={() => deleteSample.mutateAsync({
-                                    package_UID,
-                                    box_UID: item.box_UID,
-                                    sample_UID: sample.UID
-                                })}
-                        >
-                            حذف
-                        </Button>
-                    </div>
-                </>}>
-                    <Button
-                        shape="circle"
-                        type="default"
-                        style={{backgroundImage: "url(/static/pattern.png"}}
-                        className="bg-center bg-cover hover:border-8 w-36 h-36 text-sm border-8 border-primary-500 flex flex-col items-center justify-center space-y-2.5"
-                        key={index}
-                    >
-                        <Typography className="bg-white p-1 rounded-lg shadow-sm">
-                            {sample.name}
-                        </Typography>
-                    </Button>
-                </Popover>
-            ))
-        }
-
-        Array.from({length: (item?.capacity - (item.samples?.length ?? 0)) ?? 0}).map((value, index) => views.push(
-            index == 0 ?
-                <AddSample package_UID={package_UID} box_UID={item.box_UID}/> :
-                <Button
-                    shape="circle"
-                    type="dashed"
-                    disabled
-                    className="w-36 h-36 opacity-60"
-                >
-
-                </Button>
-        ))
-
-        return <Row gutter={[12, 15]}>
-            {views.map((item, index) => <Col
-                sm={12}
-                className="p-0 flex items-center justify-center"
+    if (Array.isArray(item?.samples)) {
+      item.samples?.map((sample, index) =>
+        views.push(
+          <Popover
+            title="حذف آیتم"
+            content={
+              <>
+                <Typography>
+                  از حذف ماده اولیه {sample.name} مطمئن هستید؟
+                </Typography>
+                <div className="flex justify-end">
+                  <Button
+                    danger
+                    className="mt-3 border-red-500 hover:border-red-500"
+                    loading={deleteSample.isPending}
+                    disabled={deleteSample.isPending}
+                    onClick={() =>
+                      deleteSample.mutateAsync({
+                        package_UID,
+                        box_UID: item.box_UID,
+                        sample_UID: sample.UID,
+                      })
+                    }
+                  >
+                    حذف
+                  </Button>
+                </div>
+              </>
+            }
+          >
+            <Button
+              shape="circle"
+              type="default"
+              style={{ backgroundImage: "url(/static/pattern.png" }}
+              className="bg-center bg-cover hover:border-8 w-36 h-36 text-sm border-8 border-primary-500 flex flex-col items-center justify-center space-y-2.5"
+              key={index}
             >
-                <motion.div
-                    key={index}
-                    className="flex items-center flex-col"
-                    transition={{delay: (index + cardIndex) * 0.4, duration: 0.2}}
-                    initial={{y: 20, opacity: 0}}
-                    animate={{y: 0, opacity: 1}}
-                >
-                    {item}
-                </motion.div>
-            </Col>)}
-        </Row>
+              <Typography className="bg-white p-1 rounded-lg shadow-sm">
+                {sample.name}
+              </Typography>
+            </Button>
+          </Popover>
+        )
+      );
     }
 
-
-    return boxList?.data?.map((item, index) => {
-
-        return <Col key={index} xs={24} sm={12} xl={8} xxl={6}>
-            <motion.div
-                key={index}
-                className="relative"
-                transition={{
-                    delay: index / 5,
-                }}
-                initial={{
-                    scale: 0,
-                    opacity: 0,
-                    bottom: -25,
-                    left: -10,
-                }}
-                animate={{
-                    scale: 1,
-                    opacity: 1,
-                    bottom: 0,
-                    left: 0,
-                }}
-                exit={{
-                    scale: 0,
-                    transition: {duration: 1},
-                    opacity: 0,
-                    bottom: 25,
-                }}
-            >
-                <Card
-                    className='min-h-[717px] w-full border-2 bg-gray-50 p-4 rounded-2xl space-y-4'
-                >
-                    <Typography
-                        className="font-semibold text-lg"> جعبه {["اصلی", "شاهد 1", "شاهد 2"][item.box_usage_type - 1]} </Typography>
-                    {/*<div className="min-h-[600px]">*/}
-                    {renderCircles(item, index)}
-                    </Card>
-                </motion.div>
-            </Col>
-        }
-    )
-
-};
-
-const AddSample = ({package_UID, box_UID}: { package_UID: string, box_UID: string }) => {
-
-    const available = useBoxSampleGetAvailableList({package_UID, box_UID})
-
-    const addSample = useBoxSampleAdd({package_UID, box_UID})
-
-    const [addPop, setAddPop] = useState<boolean>()
-
-    return <Popover
-        open={addPop}
-        trigger="click"
-        content={<Form
-            disabled={addSample.isPending}
-            onFinish={async (values) => {
-                const res = await addSample.mutateAsync(values)
-                if (res.success)
-                    setAddPop(false)
-            }}>
-            <Form.Item
-                name="sample_UID"
-                required={false}
-                rules={[{
-                    required: true, message: errorMessage.required_choice
-                }]}
-                className="w-96"
-                label="مواد اولیه"
-                labelCol={{span: 24}}
-            >
-                <Select
-                    className="w-full"
-                    options={available.data}
-                    fieldNames={available.fieldName}
-                />
-            </Form.Item>
-            <div className="flex justify-end">
-                <Button loading={addSample.isPending} htmlType="submit" type="primary" size="middle">
-                    ثبت
-                </Button>
-            </div>
-        </Form>} title="افزودن نمونه">
-        <Button
+    Array.from({
+      length: item?.capacity - (item.samples?.length ?? 0) ?? 0,
+    }).map((value, index) =>
+      views.push(
+        index == 0 ? (
+          <AddSample package_UID={package_UID} box_UID={item.box_UID} />
+        ) : (
+          <Button
             shape="circle"
             type="dashed"
-            className="w-36 h-36 font-medium flex flex-col items-center justify-center bg-gray-50 border-2 border-dashed border-primary-500"
+            disabled
+            className="w-36 h-36 opacity-60"
+          ></Button>
+        )
+      )
+    );
+
+    return (
+      <Row gutter={[12, 15]}>
+        {views.map((item, index) => (
+          <>
+            <Col sm={12} className="p-0 flex items-center justify-center">
+              <motion.div
+                key={index}
+                className="flex items-center flex-col"
+                transition={{ delay: (index + cardIndex) * 0.4, duration: 0.2 }}
+                initial={{ y: 20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+              >
+                {item}
+              </motion.div>
+            </Col>
+          </>
+        ))}
+      </Row>
+    );
+  };
+
+  return boxList?.data?.map((item, index) => {
+    return (
+      <Col key={index} xs={24} sm={12} xl={8} xxl={6}>
+        <motion.div
+          key={index}
+          className="relative"
+          transition={{
+            delay: index / 5,
+          }}
+          initial={{
+            scale: 0,
+            opacity: 0,
+            bottom: -25,
+            left: -10,
+          }}
+          animate={{
+            scale: 1,
+            opacity: 1,
+            bottom: 0,
+            left: 0,
+          }}
+          exit={{
+            scale: 0,
+            transition: { duration: 1 },
+            opacity: 0,
+            bottom: 25,
+          }}
         >
-            <PlusSmallIcon className="w-5 h-5"/>
-            افزودن مواد اولیه
-        </Button>
+          <Card className="min-h-[717px] w-full border-2 bg-gray-50 p-4 rounded-2xl space-y-4">
+            <Typography className="font-semibold text-lg">
+              {" "}
+              جعبه {["اصلی", "شاهد 1", "شاهد 2"][item.box_usage_type - 1]}{" "}
+            </Typography>
+            {/*<div className="min-h-[600px]">*/}
+            {renderCircles(item, index)}
+          </Card>
+        </motion.div>
+      </Col>
+    );
+  });
+};
+
+const AddSample = ({
+  package_UID,
+  box_UID,
+}: {
+  package_UID: string;
+  box_UID: string;
+}) => {
+  const available = useBoxSampleGetAvailableList({ package_UID, box_UID });
+
+  const addSample = useBoxSampleAdd({ package_UID, box_UID });
+
+  const [addPop, setAddPop] = useState<boolean>();
+
+  return (
+    <Popover
+      open={addPop}
+      trigger="click"
+      content={
+        <Form
+          disabled={addSample.isPending}
+          onFinish={async (values) => {
+            const res = await addSample.mutateAsync(values);
+            if (res.success) setAddPop(false);
+          }}
+        >
+          <Form.Item
+            name="sample_UID"
+            required={false}
+            rules={[
+              {
+                required: true,
+                message: errorMessage.required_choice,
+              },
+            ]}
+            className="w-96"
+            label="مواد اولیه"
+            labelCol={{ span: 24 }}
+          >
+            <Select
+              className="w-full"
+              options={available.data}
+              fieldNames={available.fieldName}
+            />
+          </Form.Item>
+          <div className="flex justify-end">
+            <Button
+              loading={addSample.isPending}
+              htmlType="submit"
+              type="primary"
+              size="middle"
+            >
+              ثبت
+            </Button>
+          </div>
+        </Form>
+      }
+      title="افزودن نمونه"
+    >
+      <Button
+        shape="circle"
+        type="dashed"
+        className="w-36 h-36 font-medium flex flex-col items-center justify-center bg-gray-50 border-2 border-dashed border-primary-500"
+      >
+        <PlusSmallIcon className="w-5 h-5" />
+        افزودن مواد اولیه
+      </Button>
     </Popover>
-}
+  );
+};
 export default BoxCartList;

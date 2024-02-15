03bb0a598c1a812882744cfbd008b2b52914513f
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2024-02-15 19:31:19 +0330

    fix

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,46 +1,44 @@
-"use client"
+"use client";
 
-import {Layout, Space} from 'antd/lib'
-import React, {useState} from 'react'
-import LayoutHeader from './header'
-import {Content} from 'antd/lib/layout/layout'
-import SideBar from './side-bar'
+import { Layout, Space } from "antd/lib";
+import React, { useState } from "react";
+import LayoutHeader from "./header";
+import { Content } from "antd/lib/layout/layout";
+import SideBar from "./side-bar";
 import i18next from "i18next";
 import translation from "@/lib/zod-i18n.json";
-import {zodI18nMap} from "zod-i18n-map";
-import {z} from "zod";
-import {AnimatePresence, motion} from "framer-motion";
-import {usePathname} from "next/navigation";
+import { zodI18nMap } from "zod-i18n-map";
+import { z } from "zod";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
 
-    const pathname = usePathname()
+  i18next.init({
+    lng: "fa",
+    resources: {
+      fa: { zod: translation },
+    },
+  });
 
-    i18next.init({
-        lng: "fa",
-        resources: {
-            fa: {zod: translation},
-        },
-    })
+  z.setErrorMap(zodI18nMap);
 
-    z.setErrorMap(zodI18nMap);
+  const [open, setOpen] = useState<boolean>(false);
 
-    const [open, setOpen] = useState<boolean>(false);
-
-    return (
-        <Space direction="vertical" style={{ width: "100%" }} size={[0, 48]}>
-            <Layout className="custom-layout" style={{ minHeight: "100vh" }}>
-                <LayoutHeader
-                    showDrawer={() => setOpen(prev => !prev)}
-                />
-                <Layout className="scrollable-content bg-gray-50" hasSider>
-                    <SideBar open={open} setOpen={setOpen}/>
-                    <Content
-                        className="custom-content"
-                        style={contentStyle}
-                    >
-                        <Layout className=" bg-gray-50 lg:mx-10 mx-5 mt-[125px] lg:mr-[310px] mb-8">
-                            <AnimatePresence mode="wait" initial={true}>
+  return (
+    <Space direction="vertical" style={{ width: "100%" }} size={[0, 48]}>
+      <Layout className="custom-layout" style={{ minHeight: "100vh" }}>
+        <LayoutHeader showDrawer={() => setOpen((prev) => !prev)} />
+        <Layout className="scrollable-content bg-gray-50" hasSider>
+          <SideBar open={open} setOpen={setOpen} />
+          <Content className="custom-content" style={contentStyle}>
+            <Layout className=" bg-gray-50 lg:mx-10 mx-5 mt-[125px] lg:mr-[310px] mb-8">
+              {/* <AnimatePresence mode="wait" initial={true}>
                                 <motion.div
                                     key={pathname}
                                     initial="initialState"
@@ -66,20 +64,19 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
                                 >
                             {children}
                                 </motion.div>
-                            </AnimatePresence>
-                        </Layout>
-                    </Content>
-                </Layout>
+                            </AnimatePresence> */}
             </Layout>
-        </Space>
-    )
+          </Content>
+        </Layout>
+      </Layout>
+    </Space>
+  );
 }
 
-
 const contentStyle: React.CSSProperties = {
-    position: "inherit",
-    textAlign: "center",
-    minHeight: 120,
-    lineHeight: "120px",
-    color: "#fff",
+  position: "inherit",
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
 };

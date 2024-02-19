033561943352d1be104c154ad3e531cd2c24ac62
mahdi <m.nwordpress1223@gmail.com>
2024-02-19 12:16:46 +0330

    Fix::

--- src/app/(dashboard)/(workflow)/workflow/detail/[key]/[uid]/component/common-workflow.tsx
+++ src/app/(dashboard)/(workflow)/workflow/detail/[key]/[uid]/component/common-workflow.tsx
@@ -4,6 +4,7 @@ import React, {useState} from "react";
 import useGetTask from "@/hooks/workflow-request/use-get-task";
 import useSetTask from "@/hooks/workflow-request/use-set-task";
 import {useForm} from "antd/lib/form/Form";
+import {useGetRegisteredReportsForStepByKey} from "@/hooks/material/use-get-registered-reports-for-step-by-key";
 import {useRouter} from "next/navigation";
 import {Card} from "@/components/card";
 import {Button, Divider, Input, Spin} from "antd";
@@ -12,13 +13,12 @@ import {DocumentTextIcon} from "@heroicons/react/24/outline";
 import RepostsMaker from "@/components/reposts-maker";
 import {Form} from "antd/lib";
 import WorkflowBtn from "@/components/workflow/workflow-btn";
-import {useRequestPackageReportList} from "@/hooks/request-package/use-request-package-report-list";
 
 const CommonWorkflow = ({
-  uid,
-  stepKey,
-  children,
-}: {
+                          uid,
+                          stepKey,
+                          children,
+                        }: {
   uid: string;
   stepKey: string;
   children?: React.ReactNode;
@@ -31,7 +31,7 @@ const CommonWorkflow = ({
 
   const [form] = useForm();
 
-  const reposts = useRequestPackageReportList({step_Key: stepKey, package_UID: uid});
+  const reposts = useGetRegisteredReportsForStepByKey(stepKey, uid);
 
   const router = useRouter();
 
@@ -50,63 +50,63 @@ const CommonWorkflow = ({
 
   if (!get.data && get.isFetching)
     return (
-      <Card className="min-h-[150px]">
-        <Spin />
-      </Card>
+        <Card className="min-h-[150px]">
+          <Spin/>
+        </Card>
     );
 
   return (
-    <>
-      {get.data?.task && (
-        <Breadcrumb
-          pages={[{ label: "خانه" }]}
-          currentPage={`${get.data?.task.current_Step_Name}`}
-          titleIcon={<DocumentTextIcon className="w-8" />}
-          actions={[
-            <Button key={1} size="large" onClick={() => router.back()}>
-              بازگشت
-            </Button>,
-          ]}
-        />
-      )}
-      <Card>
-        {reposts.data?.length !== 0 && (
-          <>
-            <Divider orientation="left" className="mb-6">
-              لیست گزارشات
-            </Divider>
-            <Spin spinning={get.isFetching}>
-              <RepostsMaker
-                taskId={uid}
-                reports={reposts?.data}
-                loading={reposts.isFetching}
-              />
-            </Spin>
-            <Divider />
-          </>
+      <>
+        {get.data?.task && (
+            <Breadcrumb
+                pages={[{label: "خانه"}]}
+                currentPage={`${get.data?.task.current_Step_Name}`}
+                titleIcon={<DocumentTextIcon className="w-8"/>}
+                actions={[
+                  <Button key={1} size="large" onClick={() => router.back()}>
+                    بازگشت
+                  </Button>,
+                ]}
+            />
         )}
-        {children}
-        <Form form={form} onFinish={handleSet} layout="vertical">
-          <Form.Item
-            label="توضیحات"
-            name="description"
-            required={false}
-            rules={[{ required: true, message: "لطفا مقدار را وارد نمایید" }]}
-          >
-            <Input.TextArea
-              style={{ resize: "none" }}
-              placeholder="وارد کنید" />
-          </Form.Item>
-        </Form>
-        <WorkflowBtn
-          choices={get.data?.choices}
-          onClick={(key) => {
-            form.submit();
-            setChoice(key);
-          }}
-        />
-      </Card>
-    </>
+        <Card>
+          {reposts.data?.length !== 0 && (
+              <>
+                <Divider orientation="left" className="mb-6">
+                  لیست گزارشات
+                </Divider>
+                <Spin spinning={get.isFetching}>
+                  <RepostsMaker
+                      taskId={uid}
+                      reports={reposts.data}
+                      loading={reposts.isFetching}
+                  />
+                </Spin>
+                <Divider/>
+              </>
+          )}
+          {children}
+          <Form form={form} onFinish={handleSet} layout="vertical">
+            <Form.Item
+                label="توضیحات"
+                name="description"
+                required={false}
+                rules={[{required: true, message: "لطفا مقدار را وارد نمایید"}]}
+            >
+              <Input.TextArea
+                  style={{resize: "none"}}
+                  placeholder="وارد کنید"/>
+            </Form.Item>
+          </Form>
+          <WorkflowBtn
+              choices={get.data?.choices}
+              onClick={(key) => {
+                form.submit();
+                setChoice(key);
+              }}
+          />
+        </Card>
+      </>
   );
 };
 

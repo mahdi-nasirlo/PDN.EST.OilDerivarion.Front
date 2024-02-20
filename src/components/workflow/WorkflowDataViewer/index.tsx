ec97708f0e6646b3ba6d11745278d84d82655867
mahdi <m.nwordpress1223@gmail.com>
2024-02-20 20:35:45 +0330

    Fix::

--- src/components/workflow/WorkflowDataViewer/index.tsx
+++ src/components/workflow/WorkflowDataViewer/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import {formMakerApi} from "../../../constance/form-maker";
-import {Card} from "@/components/card";
 import {ZodErrorAlert} from "@/components/zod-error-alert";
 import {Empty} from "antd";
-import {Descriptions} from "antd/lib";
+import {Descriptions, Table, TableColumnsType, Typography} from "antd/lib";
+import {Card} from "@/components/card";
 
 interface PropsType {
     data: string,
 }
 
 const Index = ({data}: PropsType) => {
-
 
     const validateData = formMakerApi.ProducerFormsGetDocSchemaByUid.type1Res.safeParse(data)
 
@@ -28,29 +27,63 @@ const Index = ({data}: PropsType) => {
 
         const view: React.ReactNode[] = []
 
-        for (let key in validateData.data.Table.Values) {
+        view.push(<Descriptions column={6}>
+            {cardData.Model?.map((item, index) => (<Descriptions.Item
+                key={index}
+                span={typeof item.Value == "string" ? 3 : 6}
+                label={item.Key}
+            >
+                {item.Value}
+            </Descriptions.Item>))}
+        </Descriptions>)
 
-            const column = cardData.Table.Header.find((item) => item.Key == key)
+
+        cardData.ListTable?.map(item => {
+
+            const columns: TableColumnsType<any> | undefined = item?.Header?.map((column) => ({
+                dataIndex: column.Key,
+                title: column.Value
+            }))
+
+            console.log(columns, item?.Values)
+            view.push(<Card className="bg-gray-100">
+                <Typography className="text-right text-lg font-semibold">
+                    {item?.Title}
+                </Typography>
+                <div className="w-full">
+                    <Table
+                        columns={columns}
+                        dataSource={item?.Values}
+                    />
+                </div>
+            </Card>)
+        })
+
+        for (let key in validateData.data.Table?.Values) {
+
+            const column = cardData.Table?.Header?.find((item) => item.Key == key)
 
             const value = cardData?.Table?.Values?.[key]
 
-            if (!column?.Hidden) view.push(<Descriptions.Item
-                span={typeof value == "string" ? 3 : 6}
-                label={column?.Value}
+            view.push(<Descriptions
+                className="text-right text-secondary-500"
+                column={6}
+                title={validateData.data.Title}
             >
-                {value}
-            </Descriptions.Item>)
+                {!column?.Hidden && column?.Value && <Descriptions.Item
+                    span={6}
+                    label={column?.Value}
+                >
+                    {value}
+                </Descriptions.Item>}
+            </Descriptions>)
 
         }
 
         return view
     }
 
-    return <Card className="bg-gray-50">
-        <Descriptions className="text-right text-secondary-500" column={6} title={validateData.data.Title}>
-            {renderCard()}
-        </Descriptions>
-    </Card>
+    return renderCard()
 };
 
 export default Index;

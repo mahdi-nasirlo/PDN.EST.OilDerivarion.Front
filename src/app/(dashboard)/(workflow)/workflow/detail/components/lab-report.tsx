a1f77e956b1fc13b443c19f0b54bed3006ce7f63
mahdi <m.nwordpress1223@gmail.com>
2024-02-29 23:45:03 +0330

    Fix::

--- src/app/(dashboard)/(workflow)/workflow/detail/components/lab-report.tsx
+++ src/app/(dashboard)/(workflow)/workflow/detail/components/lab-report.tsx
@@ -18,18 +18,15 @@ export default function LabResult({ package_UID }: { package_UID: string }) {
                     className='bg-gray-50'
                 >
                     <Descriptions
-                        column={6}
+                        column={4}
                         className='text-right text-secondary-500'
                         title={`${result.Sample_Name} _ ${result.Sample_Type_Value}`}
                     >
                         <Descriptions.Item span={2} label="عنوان آزمون">
                             {result.Name}
                         </Descriptions.Item>
-                        <Descriptions.Item span={3} label="استاندارد آزمون">
+                        <Descriptions.Item span={2} label="استاندارد آزمون">
                             {result.Title}
-                        </Descriptions.Item>
-                        <Descriptions.Item span={6} label="توضیحات">
-                            {result.Result_Desc}
                         </Descriptions.Item>
                         <Descriptions.Item span={2} label="نتیجه آزمایش">
                             {result.Result_Test}
@@ -46,33 +43,38 @@ export default function LabResult({ package_UID }: { package_UID: string }) {
                         <Descriptions.Item span={2} label="مقدار تجدید پذیری">
                             {result.Result_Renewable}
                         </Descriptions.Item>
-                        <Descriptions.Item span={2} label="نام تامین کننده">
-                            {result.Material_Supply_Name}
-                        </Descriptions.Item>
-                        <Descriptions.Item span={3} label="شماره اظهار نامه">
-                            {result.Material_Import_Declaration_Number}
-                        </Descriptions.Item>
-                        <Descriptions.Item span={3} label="کد ملی تامین کننده">
-                            {result.Material_Supply_National_Code}
-                        </Descriptions.Item>
-                        <Descriptions.Item span={3} label="در صد استفاده ماده اولیه">
-                            {result.Material_Usage_Percentage}
-                        </Descriptions.Item>
-                        <Descriptions.Item span={6} label="میزان مصرف کل برای یک واحد تولیدی(کیلوگرم)">
-                            {result.Material_Unit_Consumption}
-                        </Descriptions.Item>
-                        <Descriptions.Item label="تعداد فاکتور آزمون">
-                            {result.Test_Item_Count}
-                        </Descriptions.Item>
-                        <Descriptions.Item span={6} label="ایرانکد">
-                            {result.Material_Supply_Iran_Code}
-                        </Descriptions.Item>
-                        <Descriptions.Item span={6} label="آدرس تامین کننده">
-                            {result.Material_Supply_Address}
-                        </Descriptions.Item>
-                        <Descriptions.Item span={3} label="تجدید پذیر">
+                        <Descriptions.Item span={2} label="تجدید پذیر">
                             {result.Result_Renewable}
                         </Descriptions.Item>
+                        <Descriptions.Item span={4} label="توضیحات">
+                            {result.Result_Desc}
+                        </Descriptions.Item>
+                        {!result.Is_Product && <>
+                            <Descriptions.Item span={2} label="نام تامین کننده">
+                                {result.Material_Supply_Name}
+                            </Descriptions.Item>
+                            <Descriptions.Item span={2} label="کد ملی تامین کننده">
+                                {result.Material_Supply_National_Code}
+                            </Descriptions.Item>
+                            <Descriptions.Item span={2} label="ایرانکد">
+                                {result.Material_Supply_Iran_Code}
+                            </Descriptions.Item>
+                            <Descriptions.Item span={2} label="شماره اظهار نامه">
+                                {result.Material_Import_Declaration_Number}
+                            </Descriptions.Item>
+                            <Descriptions.Item span={2} label="در صد استفاده ماده اولیه">
+                                {result.Material_Usage_Percentage}
+                            </Descriptions.Item>
+                            <Descriptions.Item span={2} label="میزان مصرف کل برای یک واحد تولیدی(کیلوگرم)">
+                                {result.Material_Unit_Consumption}
+                            </Descriptions.Item>
+                            <Descriptions.Item span={4} label="آدرس تامین کننده">
+                                {result.Material_Supply_Address}
+                            </Descriptions.Item>
+                            <Descriptions.Item label="تعداد فاکتور آزمون">
+                                {result.Test_Item_Count}
+                            </Descriptions.Item>
+                        </>}
                     </Descriptions>
                 </Card>
             </>)}
